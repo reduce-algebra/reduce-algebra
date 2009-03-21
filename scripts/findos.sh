0cec84831f550cb20ccf4b0e8f2eb0f20ee2cfc6
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2009-03-21 16:49:28 +0000

    use lsb_release if available
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@171 2bfe0521-f11c-4a00-b80e-6202646ff360

--- scripts/findos.sh
+++ scripts/findos.sh
@@ -13,12 +13,6 @@
 
 os="unknown"
 
-# I think it is probable that if lsb_release is available  the result it
-# provides will be more reliable than looking in /etc/issue
-
-vendor=`if { lsb_release -d ; } 2>/dev/null; then : ; else echo unknown; fi`
-version=`if { lsb_release -r ; } 2>/dev/null; then : ; else echo unknown; fi` 
-
 case `uname -a` in
 *CYGWIN* | *Cygwin* | *cygwin* | *MINGW* | *MinGW* | *Mingw* | *mingw*)
   echo "windows"
@@ -30,8 +24,52 @@ FreeBSD*)
   ;;
 esac
 
-if test -f /etc/issue
+vendor=`if { lsb_release -d ; } 2>/dev/null; then : ; else echo unknown; fi`
+version=`if { lsb_release -r ; } 2>/dev/null; then : ; else echo unknown; fi` 
+
+# I think it is probable that if lsb_release is available  the result it
+# provides will be more reliable than looking in /etc/issue
+
+if test "x$vendor" != "xunknown"
 then
+# I normalise the names of distributions I know about...
+  case $vendor in
+  *Debian*)
+    vendor="debian"
+    ;;
+  *Fedora*)
+    vendor="fedora"
+    ;;
+  *SUSE*)
+    vendor="suse"
+    ;;
+  *Ubuntu*)
+    vendor="ubuntu"
+    ;;
+  *Scientific*)
+    vendor="scientificlinux"
+    ;;
+# Add more distributions here please!
+  *)
+    vendor="unknown"
+    ;;
+  esac
+fi
+
+
+
+if "x$vendor" != "xunknown"
+then
+  if "x$version" != "xunknown"
+  then
+    version=`echo $version | sed '-e s/[^:]*:[ \t]*//'`
+    os="$vendor$version"
+  else
+    os="$vendor"
+  end
+else
+  if test -f /etc/issue
+  then
 # I will detect a bunch of the bigger distributions. The
 # first thing to do is to spot the main family, after that
 # I will try to decode the release number.
@@ -42,60 +80,64 @@ then
 # messing about with "head" etc. Actuall some of the pain is that on Ubuntu
 # the file /etc/issue contains a string "\n" that sometimes gets mapped
 # onto a newline.
-  issue=`cat /etc/issue`
-  hissue=`echo "$issue" | head -1`
-  if test "x$hissue" = "x"
-  then
-    hissue="$issue"
-  fi
-  case $issue in
-  *Red*Hat*)
-    os=`echo $hissue | sed 's/^[^0-9]*\\([0-9][0-9]*\\.*[0-9]*\\).*$/rh\\1/'`
-    ;;
-  *SUSE*)
-    os=`echo $hissue | sed 's/^[^0-9]*\\([0-9][0-9]*\\.*[0-9]*\\).*$/suse\\1/'`
-    ;;
-  *Fedora*)
-    os=`echo $hissue | sed 's/^[^0-9]*\\([0-9][0-9]*\\.*[0-9]*\\).*$/fedora\\1/'`
-    ;;
-  *Scientific*Linux*)
-    os=`echo $hissue | sed 's/^[^0-9]*\\([0-9][0-9]*\\.*[0-9]*\\).*$/scientificlinux\\1/'`
-    ;;
-  *Debian*)
-    os=`echo $hissue | sed 's/^[^0-9]*\\([0-9][0-9]*\\.*[0-9]*\\).*$/debian\\1/'`
-    ;;
-  *Ubuntu*)
-    os=`echo $hissue | sed 's/^[^0-9]*\\([0-9][0-9]*\\.*[0-9]*\\).*$/ubuntu\\1/'`
-    ;;
-  *Mandriva*)
-    os=`echo $hissue | sed 's/^[^0-9]*\\([0-9][0-9]*\\.*[0-9]*\\).*$/mandriva\\1/'`
-    ;;
-  *Mandrake*)
-    os=`echo $hissue | sed 's/^[^0-9]*\\([0-9][0-9]*\\.*[0-9]*\\).*$/mandrake\\1/'`
-    ;;
-  esac
-else
-  if test -f /System/Library/CoreServices/SystemVersion.plist
-  then
-# For MacOS I will detect the version number and report a code-name for it.
-    case `cat /System/Library/CoreServices/SystemVersion.plist` in
-    *Mac*OS*X*ProductVersion*\<string\>10.2*)
-      os="mac_10.2_jaguar"
+    issue=`cat /etc/issue`
+    hissue=`echo "$issue" | head -1`
+    if test "x$hissue" = "x"
+    then
+      hissue="$issue"
+    fi
+    case $issue in
+    *Red*Hat*)
+      os=`echo $hissue | sed 's/^[^0-9]*\\([0-9][0-9]*\\.*[0-9]*\\).*$/rh\\1/'`
       ;;
-    *Mac*OS*X*ProductVersion*\<string\>10.3*)
-      os="mac_10.3_panther"
+    *SUSE*)
+      os=`echo $hissue | sed 's/^[^0-9]*\\([0-9][0-9]*\\.*[0-9]*\\).*$/suse\\1/'`
       ;;
-    *Mac*OS*X*ProductVersion*\<string\>10.4*)
-      os="mac_10.4_tiger"
+    *Fedora*)
+      os=`echo $hissue | sed 's/^[^0-9]*\\([0-9][0-9]*\\.*[0-9]*\\).*$/fedora\\1/'`
       ;;
-    *Mac*OS*X*ProductVersion*\<string\>10.5*)
-      os="mac_10.5_leopard"
+    *Scientific*Linux*)
+      os=`echo $hissue | sed 's/^[^0-9]*\\([0-9][0-9]*\\.*[0-9]*\\).*$/scientificlinux\\1/'`
+      ;;
+    *Debian*)
+      os=`echo $hissue | sed 's/^[^0-9]*\\([0-9][0-9]*\\.*[0-9]*\\).*$/debian\\1/'`
+      ;;
+    *Ubuntu*)
+      os=`echo $hissue | sed 's/^[^0-9]*\\([0-9][0-9]*\\.*[0-9]*\\).*$/ubuntu\\1/'`
+      ;;
+    *Mandriva*)
+      os=`echo $hissue | sed 's/^[^0-9]*\\([0-9][0-9]*\\.*[0-9]*\\).*$/mandriva\\1/'`
+      ;;
+    *Mandrake*)
+      os=`echo $hissue | sed 's/^[^0-9]*\\([0-9][0-9]*\\.*[0-9]*\\).*$/mandrake\\1/'`
       ;;
     esac
   else
-    os="unknown"
+    if test -f /System/Library/CoreServices/SystemVersion.plist
+    then
+# For MacOS I will detect the version number and report a code-name for it.
+      case `cat /System/Library/CoreServices/SystemVersion.plist` in
+      *Mac*OS*X*ProductVersion*\<string\>10.2*)
+        os="mac_10.2_jaguar"
+        ;;
+      *Mac*OS*X*ProductVersion*\<string\>10.3*)
+        os="mac_10.3_panther"
+        ;;
+      *Mac*OS*X*ProductVersion*\<string\>10.4*)
+        os="mac_10.4_tiger"
+        ;;
+      *Mac*OS*X*ProductVersion*\<string\>10.5*)
+        os="mac_10.5_leopard"
+        ;;
+      esac
+    else
+      os="unknown"
+    fi
   fi
 fi
+
+# I want to ensure that there are no blanks, table or slashed in the name
+os = `echo $os | sed -e 's/[ \t/]//g'`
 
 echo $os
 
