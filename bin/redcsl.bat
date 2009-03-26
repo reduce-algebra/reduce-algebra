d0f93eb61ea3e04de1d4f6f12456771d4db48185
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2009-03-26 20:53:55 +0000

    cope with rename from reduce to redcsl
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@203 2bfe0521-f11c-4a00-b80e-6202646ff360

--- bin/redcsl.bat
+++ bin/redcsl.bat
@@ -3,26 +3,26 @@
 set h=%~dp0
 set n=%~n0
 
-if exist %h%..\cslbuild\x86_64-pc-windows\csl\%n%.com       goto :x64
-if exist %h%..\cslbuild\i686-pc-windows\csl\%n%.com         goto :x86
-if exist %h%..\cslbuild\x86_64-pc-windows-debug\csl\%n%.com goto :x64d
-if exist %h%..\cslbuild\i686-pc-windows-debug\csl\%n%.com   goto x86d
+if exist %h%..\cslbuild\x86_64-pc-windows\csl\reduce.com       goto :x64
+if exist %h%..\cslbuild\i686-pc-windows\csl\reduce.com         goto :x86
+if exist %h%..\cslbuild\x86_64-pc-windows-debug\csl\reduce.com goto :x64d
+if exist %h%..\cslbuild\i686-pc-windows-debug\csl\reduce.com   goto :x86d
 
-echo +++ No ready-compiled %n% available
-echo Go "./configure; make" in a cygwin shell please
+echo +++ No ready-compiled reduce available
+echo Go "./configure --with-csl; make" in a cygwin shell please
 goto :done
 
 :x64
-%h%..\scripts\run1 %h%..\cslbuild\x86_64-pc-windows\csl\%n%.com %n% %*
+%h%..\scripts\run1 %h%..\cslbuild\x86_64-pc-windows\csl\reduce.com redcsl %*
 
 :x86
-%h%..\scripts\run1 %h%..\cslbuild\i686-pc-windows\csl\%n%.com %n% %*
+%h%..\scripts\run1 %h%..\cslbuild\i686-pc-windows\csl\reduce.com redcsl %*
 
 :x64d
-%h%..\scripts\run1 %h%..\cslbuild\x86_64-pc-windows-debug\csl\%n%.com %n% %*
+%h%..\scripts\run1 %h%..\cslbuild\x86_64-pc-windows-debug\csl\reduce.com redcsl %*
 
 :x86d
-%h%..\scripts\run1 %h%..\cslbuild\i686-pc-windows-debug\csl\%n%.com %n% %*
+%h%..\scripts\run1 %h%..\cslbuild\i686-pc-windows-debug\csl\reduce.com redcsl %*
 
 :done
 
