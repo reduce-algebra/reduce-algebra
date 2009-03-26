9d4f42e5322321c397b64fd3487680b35798cb71
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2009-03-26 22:12:14 +0000

    non-cached but maybe starting to work
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@205 2bfe0521-f11c-4a00-b80e-6202646ff360

--- scripts/runpsl.sh
+++ scripts/runpsl.sh
@@ -86,7 +86,7 @@ else
       bin="$here/../pslbuild/$host$hx/psl/$ap"
 #     rm -f $here/../bin/$scr
 #     ln -s $bin $here/../bin/$scr
-      exec $bin -td $STORE -f $here/../pslbuild/$hx/red/reduce.img $*
+      exec $bin -td $STORE -f $here/../pslbuild/$host$hx/red/reduce.img $*
       exit 0
     fi
   done
