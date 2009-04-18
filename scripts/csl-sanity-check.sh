bdadbaddf136102f4dbb1ce26952e82a7e2f7bdd
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2009-04-18 21:50:36 +0000

    initial checkin
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@252 2bfe0521-f11c-4a00-b80e-6202646ff360

---
+++ scripts/csl-sanity-check.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+
+# This tries to configure, compile and then run a teeny windowed
+# program. If this FAILS then it is probable that any attempt to compile
+# the full CSL version of Reduce will also give trouble.
+
+
+# I want this script to be one I can launch from anywhere, but at least
+# some of its sub-scripts will not be so generous. So find out where it
+# lives so that other locations can be found relative to that.
+
+a=$0
+c=unknown
+case $a in
+/* )
+  c=$a  
+  ;;
+*/* )
+  case $a in
+  ./* )
+    a=${a#./}
+    ;;
+  esac
+  c=`pwd`/$a
+  ;;
+* ) 
+  for d in $PATH
+  do
+    if test -x $d/$a
+    then
+      c=$d/$a
+    fi
+  done
+  if test $c = "unknown" ;then
+    echo "Unable to find full path for script. Please re-try"
+    echo "launching it using a fully rooted path."
+    exit 1
+  fi
+  ;;
+esac
+
+here=${c%/*}
+here=${here%/*}
+
+if ! test -d $here/cslbuild
+then
+  mkdir -p $here/cslbuild
+fi
+
+cd $here/cslbuild
+../csl/configure && make && exec ./testprogram
+
