46825e24e6550c4ac14cadc63250c2d93571fa8b
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2009-04-25 19:26:43 +0000

    look for fallback case if no exact OS match is available
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@277 2bfe0521-f11c-4a00-b80e-6202646ff360

--- scripts/run.sh
+++ scripts/run.sh
@@ -47,8 +47,8 @@ scr="$2"
 shift
 shift
 
-host=`$here/../config.guess`
-host=`$here/findhost.sh $host`
+host0=`$here/../config.guess`
+host=`$here/findhost.sh $host0`
 
 # If I am on a Windows machine and I have built a Windows-64 system
 # I should try that too. On windows the "-m32" and "-m64" flags do
@@ -95,6 +95,37 @@ else
       exit 0
     fi
   done
+
+# Here a there does not seem to be a version made for the EXACT operating
+# system version that we are running on at present. So to try to be kind I
+# will try to see if there is one for a version that may be related. In this
+# case "related" should mean using the same CPU (eg as in i686 vs x86_64)
+# and with the same "OS vendor" (eg debian, ubuntu, fedora etc). The effect
+# is intended to be that if you have installed say a version intended for
+# ubuntu 8.10 while in fact you are now running ubuntu 9.04 then things
+# will work. But because there is a potential conflict here (eg a version
+# built for fedora 9 might now work on fedora 2) I will display an alert 
+# about the possible issue. Furthermore in this case (because I would like
+# you to install a version exactly matching your operating system) I will
+# not update the run script and so will do the tedious search every time.
+
+  host1=`$here/findhost.sh $host0 --short`
+  for hx in $here/../cslbuild/${host1}*
+  do
+    if test -x $hx/csl/$ap
+    then
+      echo "You are running $host but the nearest match"
+      echo "I can find was built for ${hx##*/}. I will try"
+      echo "it, but if there are problems you need to compile"
+      echo "a version for yourself using \"./configure; make\"".
+      bin="$hx/csl/$ap"
+#     rm -f $here/../bin/$scr
+#     ln -s -f $bin $here/../bin/$scr
+      exec $bin $*
+      exit 0
+    fi
+  done
+
 fi
 
 echo Failed to find a version of $ap that you have built
