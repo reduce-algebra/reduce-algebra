cb3c9e9571e8ea01a9b015c29e966f093af452f6
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2009-07-12 10:07:49 +0000

    remove scheme that caused run scripts to update themselves on first use
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@377 2bfe0521-f11c-4a00-b80e-6202646ff360

--- bin/reduce.bat
+++ bin/reduce.bat
@@ -9,20 +9,24 @@ if exist %h%..\cslbuild\x86_64-pc-windows-debug\csl\%n%.com goto :x64d
 if exist %h%..\cslbuild\i686-pc-windows-debug\csl\%n%.com   goto x86d
 
 echo +++ No ready-compiled %n% available
-echo Go "./configure; make" in a cygwin shell please
+echo Go "./configure --with-csl; make" in a cygwin shell please
 goto :done
 
 :x64
-%h%..\scripts\run1 %h%..\cslbuild\x86_64-pc-windows\csl\%n%.com %n% %*
+%h%..\cslbuild\x86_64-pc-windows\csl\%n%.com %*
+goto :done
 
 :x86
-%h%..\scripts\run1 %h%..\cslbuild\i686-pc-windows\csl\%n%.com %n% %*
+%h%..\cslbuild\i686-pc-windows\csl\%n%.com %*
+goto :done
 
 :x64d
-%h%..\scripts\run1 %h%..\cslbuild\x86_64-pc-windows-debug\csl\%n%.com %n% %*
+%h%..\cslbuild\x86_64-pc-windows-debug\csl\%n%.com %*
+goto :done
 
 :x86d
-%h%..\scripts\run1 %h%..\cslbuild\i686-pc-windows-debug\csl\%n%.com %n% %*
+%h%..\cslbuild\i686-pc-windows-debug\csl\%n%.com %*
+goto :done
 
 :done
 
