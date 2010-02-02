771f523efef5db051444a8ff58e4088eb095505f
Winfried Neun <winfriedneun@users.sourceforge.net>
2010-02-02 12:48:41 +0000

    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@538 2bfe0521-f11c-4a00-b80e-6202646ff360

--- psl/dist/kernel/macintel64/bpsheap.c
+++ psl/dist/kernel/macintel64/bpsheap.c
@@ -9,29 +9,7 @@
 % Mode:         Text
 % Package:
 %
-% (c) Copyright 1982, University of Utah
-%
-% Redistribution and use in source and binary forms, with or without
-% modification, are permitted provided that the following conditions are met:
-%
-%    * Redistributions of source code must retain the relevant copyright
-%      notice, this list of conditions and the following disclaimer.
-%    * Redistributions in binary form must reproduce the above copyright
-%      notice, this list of conditions and the following disclaimer in the
-%      documentation and/or other materials provided with the distribution.
-%
-% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-% THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-% PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNERS OR
-% CONTRIBUTORS
-% BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-% POSSIBILITY OF SUCH DAMAGE.
+% (c) Copyright 1987, University of Utah, all rights reserved.
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
@@ -233,7 +211,7 @@ printf("total %lx %lx %x\n",heapsize_in_bytes , current_size_in_bytes,total);
         hl =  heaplast; htb = heaptrapbound;
     /* save the new values around restore of the old ones */
 
-       printf("Loading image file :%s \n",imagefile); 
+       printf("Loading image file: %s \n",imagefile); 
        imago = fopen (imagefile,"r");
        if (imago == NULL) { perror ("error"); exit (-1); }
        fread (headerword,8,2,imago);
@@ -285,7 +263,6 @@ setupbps()
   char c;
 
   nextbps  =  ((long long) bps + 7) & ~7;        /* Up to a multiple of 8. */
-printf("setupbps %lx, %lx\n",nextbps,bps);
   bpslowerbound = nextbps;
   lastbps  =  ((long long)bps + BPSSIZE) & ~7;    /* Down to a multiple of 8. */
   p = (char *)(( bpslowerbound  -1) & ~(PAGESIZE-1));
@@ -328,10 +305,7 @@ allocatemorebps()
 getheap(heapsize)
      long long heapsize;
 {
-printf("%d %x\n",heapsize,heapsize);
-
   heaplowerbound        = (long long )calloc(heapsize/512 +1,1024);  /* allocate first heap */;
-  printf("calloc %ld * 1024\n",heapsize/512 +1);
 
   if ((int) heaplowerbound == 0) {
     perror("GETHEAP");
