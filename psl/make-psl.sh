7e0080fe0ae8315bcd3a25746e1283e041476ac9
Winfried Neun <winfriedneun@users.sourceforge.net>
2010-10-28 15:55:59 +0000

    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@824 2bfe0521-f11c-4a00-b80e-6202646ff360

---
+++ psl/make-psl.sh
@@ -0,0 +1,26 @@
+#! /bin/sh -x
+#   Makes PSL in the reduce-algebra tree
+#   uses config-guess   and the position in the file tree (PROOT)
+#   the MACHINE variable ist set according to the old PSL
+#   names derived fron the config-guess.
+
+export PROOT=`pwd`
+
+guess=`../config.guess`
+
+
+if test $guess = "x86_64-unknown-linux-gnu"
+ then
+	export MACHINE=AMD64
+fi
+
+echo "make for PSL in "$PROOT " for MACHINE " $MACHINE
+
+psys=$PROOT/bin/$MACHINE
+pxk=$PROOT/dist/kernel/$MACHINE
+pl=$PROOT/dist/lap/$MACHINE
+pnkl=$PROOT/dist/nonkernel/$MACHINE/lap
+
+cd dist
+make
+
