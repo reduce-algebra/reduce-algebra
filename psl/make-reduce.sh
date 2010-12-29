1e9666f803f5df7ddaa353902ac91f9c548fb7f1
Winfried Neun <winfriedneun@users.sourceforge.net>
2010-12-29 16:39:27 +0000

    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@1059 2bfe0521-f11c-4a00-b80e-6202646ff360

--- psl/make-reduce.sh
+++ psl/make-reduce.sh
@@ -67,8 +67,9 @@ mkdir buildlogs
 cp -r ../$host/Makefile .
 cp ../../psl/dist/nonkernel/$MACHINE/lap/* psl
 cp ../../psl/dist/lap/$MACHINE/* psl
-cp ../..//psl/dist/kernel/$MACHINE/bpsl psl/
+cp ../../psl/dist/kernel/$MACHINE/bpsl psl/
 rm -f red/*.img
+touch ../../psl/mkreduce
 cp psl/nbig32.b psl/zbig.b
 rm -f psl/*rlisp*.b
 make
