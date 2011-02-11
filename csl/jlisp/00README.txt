785079b7d38f4f2964175f26cb08b8c8a219dc9c
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2011-02-11 12:53:58 +0000

    Explicit BSD license added to jlisp source files
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@1115 2bfe0521-f11c-4a00-b80e-6202646ff360

--- csl/jlisp/00README.txt
+++ csl/jlisp/00README.txt
@@ -1,17 +1,10 @@
-JLISP
+                                  Jlisp
+                                  =====
 
-This code is now OLD and NOT SUPPORTED.
 
-Anybody who wishes to use it can hack it for themselves. It dates at
-best from the times of Reduce version 3.7 and it HAS NOT been updated to
-reflect the file layout and so on of the current Open Source system or
-to incorporate package contributed since Reduce 3.7.
 
-To try it you may just try
-   java -jar jlisp.jar
-to use a pre-built version!
+This is in effect a clone of CSL coded in Java. 
 
-Good luck. Please tell me if you get anywhere bringing this up to date.
 
               Arthur Norman
-              July 2010
+              February 2011
--- csl/jlisp/CWin.java
+++ csl/jlisp/CWin.java
@@ -1,3 +1,34 @@
+/**************************************************************************
+ * Copyright (C) 1998-2011, Codemist Ltd.                A C Norman       *
+ *                            also contributions from Vijay Chauhan, 2002 *
+ *                                                                        *
+ * Redistribution and use in source and binary forms, with or without     *
+ * modification, are permitted provided that the following conditions are *
+ * met:                                                                   *
+ *                                                                        *
+ *     * Redistributions of source code must retain the relevant          *
+ *       copyright notice, this list of conditions and the following      *
+ *       disclaimer.                                                      *
+ *     * Redistributions in binary form must reproduce the above          *
+ *       copyright notice, this list of conditions and the following      *
+ *       disclaimer in the documentation and/or other materials provided  *
+ *       with the distribution.                                           *
+ *                                                                        *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS    *
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT      *
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS      *
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE         *
+ * COPYRIGHT OWNERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,   *
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,   *
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS  *
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND *
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR  *
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF     *
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH   *
+ * DAMAGE.                                                                *
+ *************************************************************************/
+
+
 //
 // CWin.java                              Copyright A C Norman, 2000
 //
@@ -452,11 +483,12 @@ public void init()
     {   public void actionPerformed(ActionEvent e)
         {   try
             {   JFileChooser ch = new JFileChooser("");
-                ExtensionFileFilter f =
-                    new ExtensionFileFilter(
-                        new String [] {"red", "tst"},
-                        "REDUCE source and test files");
-                ch.setFileFilter(f);
+// Can this now be FileNameExtensionFilter ????
+//              ExtensionFileFilter f =
+//                  new ExtensionFileFilter(
+//                      new String [] {"red", "tst"},
+//                      "REDUCE source and test files");
+//              ch.setFileFilter(f);
                 int r = ch.showOpenDialog(container);
                 if (r == JFileChooser.APPROVE_OPTION)
                 {   String fileName = ch.getSelectedFile().getAbsolutePath();
