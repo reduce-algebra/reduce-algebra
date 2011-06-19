a1309a91e9c12097e5a8e1bdbab1b636fda499fa
Ted Kosan <tkosan@users.sourceforge.net>
2011-06-19 17:20:49 +0000

    Added two more GWT .jar files to the classpath in compile.bat and compile.sh. The .jar files are needed to fix the validation-related errors that are currently being generated when the CAS is compiled into JavaScript. Also updated README to reflect this change.
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@1204 2bfe0521-f11c-4a00-b80e-6202646ff360

--- jslisp/lib/google_web_toolkit/compile.sh
+++ jslisp/lib/google_web_toolkit/compile.sh
@@ -1,1 +1,1 @@
-java -cp ../../../lib/gwt-dev.jar:../../../lib/gwt-user.jar:./src com.google.gwt.dev.Compiler -logLevel ALL org.mathpiper.module
+java -cp ../../../lib/gwt-dev.jar:../../../lib/gwt-user.jar:../../../lib/validation-api-1.0.0.GA.jar:../../../lib/validation-api-1.0.0.GA-sources.jar:./src com.google.gwt.dev.Compiler -logLevel ALL org.mathpiper.module
