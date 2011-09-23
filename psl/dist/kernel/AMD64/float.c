d720654e907a944d4a1b4fc31d4bb4007576fb90
Winfried Neun <winfriedneun@users.sourceforge.net>
2011-09-23 10:11:44 +0000

    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@1419 2bfe0521-f11c-4a00-b80e-6202646ff360

--- psl/dist/kernel/AMD64/float.c
+++ psl/dist/kernel/AMD64/float.c
@@ -61,7 +61,7 @@ uxfloat(f,i)
 
 /* Tag( uxfix )
  */
-int uxfix(f)
+long long uxfix(f)
      double *f;
 {
   return *f;
@@ -75,12 +75,17 @@ uxassign(f1,f2)
   *f1 = *f2;
 }
 
+fexcept_t flagp;
+
 /* Tag( uxplus2 )
  */
 uxplus2(f1,f2,f3)
      double *f1, *f2, *f3;
 {
   *f1 = *f2 + *f3;
+  fegetexceptflag(&flagp, FE_OVERFLOW | FE_DIVBYZERO);
+  if(flagp != 0) {feclearexcept(FE_OVERFLOW | FE_DIVBYZERO); return (0);}
+  return (1);
 }
 
 /* Tag( uxdifference )
@@ -89,9 +94,10 @@ uxdifference(f1,f2,f3)
      double *f1, *f2, *f3;
 {
   *f1 = *f2 - *f3;
+  fegetexceptflag(&flagp, FE_OVERFLOW | FE_DIVBYZERO);
+  if(flagp != 0) {feclearexcept(FE_OVERFLOW | FE_DIVBYZERO); return (0);}
+  return (1);
 }
-
-fexcept_t flagp;
 
 /* Tag( uxtimes2 )
  */
@@ -155,7 +161,7 @@ uxwritefloat(buf, flt, convstr)
      char *convstr;      /* String containing conversion field for sprintf */
 {
   char *temps, *dot, *e;
-  char tempbuf[100]; /* reasonable size limit */
+  char tempbuf [100]; /* reasonable size limit */
   float  tempf;
 
   temps = buf + 8;       /* Skip over lisp string length to write data */
@@ -208,58 +214,89 @@ uxsin (r, x)
      double *r, *x;
 {
     *r = sin( *x );
+  fegetexceptflag(&flagp, FE_OVERFLOW | FE_DIVBYZERO);
+  if(flagp != 0) {feclearexcept(FE_OVERFLOW | FE_DIVBYZERO); return (0);}
+  return (1);
 }
 
 uxcos (r, x)
      double *r, *x;
 {
     *r = cos( *x );
+  fegetexceptflag(&flagp, FE_OVERFLOW | FE_DIVBYZERO);
+  if(flagp != 0) {feclearexcept(FE_OVERFLOW | FE_DIVBYZERO); return (0);}
+  return (1);
 }
 
 uxtan (r, x)
      double *r, *x;
 {
     *r = tan( *x );
+  fegetexceptflag(&flagp, FE_OVERFLOW | FE_DIVBYZERO);
+  if(flagp != 0) {feclearexcept(FE_OVERFLOW | FE_DIVBYZERO); return (0);}
+  return (1);
+
 }
 
 uxasin (r, x)
      double *r, *x;
 {
     *r = asin( *x );
+  fegetexceptflag(&flagp, FE_OVERFLOW | FE_DIVBYZERO);
+  if(flagp != 0) {feclearexcept(FE_OVERFLOW | FE_DIVBYZERO); return (0);}
+  return (1);
 }
 
 uxacos (r, x)
      double *r, *x;
 {
     *r = acos( *x );
+  fegetexceptflag(&flagp, FE_OVERFLOW | FE_DIVBYZERO);
+  if(flagp != 0) {feclearexcept(FE_OVERFLOW | FE_DIVBYZERO); return (0);}
+  return (1);
 }
 
 uxatan (r, x)
      double *r, *x;
 {
     *r = atan( *x );
+  fegetexceptflag(&flagp, FE_OVERFLOW | FE_DIVBYZERO);
+  if(flagp != 0) {feclearexcept(FE_OVERFLOW | FE_DIVBYZERO); return (0);}
+  return (1);
 }
 
 uxsqrt (r, x)
      double *r, *x;
 {
     *r = sqrt( *x );
+  fegetexceptflag(&flagp, FE_OVERFLOW | FE_DIVBYZERO);
+  if(flagp != 0) {feclearexcept(FE_OVERFLOW | FE_DIVBYZERO); return (0);}
+  return (1);
 }
 
 uxexp (r, x)
      double *r, *x;
 {
     *r = exp( *x );
+  fegetexceptflag(&flagp, FE_OVERFLOW | FE_DIVBYZERO);
+  if(flagp != 0) {feclearexcept(FE_OVERFLOW | FE_DIVBYZERO); return (0);}
+  return (1);
 }
 
 uxlog (r, x)
      double *r, *x;
 {
     *r = log( *x );
+  fegetexceptflag(&flagp, FE_OVERFLOW | FE_DIVBYZERO);
+  if(flagp != 0) {feclearexcept(FE_OVERFLOW | FE_DIVBYZERO); return (0);}
+  return (1);
 }
 
 uxatan2 (r, y, x)
      double *r, *y, *x;
 {
     *r = atan2( *y, *x );
+  fegetexceptflag(&flagp, FE_OVERFLOW | FE_DIVBYZERO);
+  if(flagp != 0) {feclearexcept(FE_OVERFLOW | FE_DIVBYZERO); return (0);}
+  return (1);
 }
