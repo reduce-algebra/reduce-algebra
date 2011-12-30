bd4ab7c87e10435149551c2585dcfff17081e409
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2011-12-30 10:05:16 +0000

    scripts to maintain xmpl directory
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@1540 2bfe0521-f11c-4a00-b80e-6202646ff360

---
+++ scripts/copy-to-xmpl.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+# I want this script to be one I can launch from anywhere, but at least
+# some of its sub-scripts will not be so generous. So find out where it
+# lives so that other locations can be found relative to that.
+
+a=$0
+c=unknown
+case $a in
+/* )
+  c=$a  
+  ;;
+*/* )
+  case $a in
+  ./* )
+    a=`echo $a | sed -e s+./++`
+    ;;
+  esac
+  c=`pwd`/$a
+  ;;
+* ) 
+  for d in $PATH
+  do
+    if test -x $d/$a
+    then
+      c=$d/$a
+    fi
+  done
+  if test $c = "unknown" ;then
+    echo "Unable to find full path for script. Please re-try"
+    echo "launching it using a fully rooted path."
+    exit 1
+  fi
+  ;;
+esac
+
+here=`echo $c | sed -e 's+/[^/]*$++'`
+here=`echo $here | sed -e 's+/[^/]*$++'`
+
+for x in $*
+do
+echo echo  $x "=>" ${x##*/}
+cp $x $here/xmpl/${x##*/}
+done
+
+
+
---
+++ scripts/update-xmpl.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+# I want this script to be one I can launch from anywhere, but at least
+# some of its sub-scripts will not be so generous. So find out where it
+# lives so that other locations can be found relative to that.
+
+a=$0
+c=unknown
+case $a in
+/* )
+  c=$a  
+  ;;
+*/* )
+  case $a in
+  ./* )
+    a=`echo $a | sed -e s+./++`
+    ;;
+  esac
+  c=`pwd`/$a
+  ;;
+* ) 
+  for d in $PATH
+  do
+    if test -x $d/$a
+    then
+      c=$d/$a
+    fi
+  done
+  if test $c = "unknown" ;then
+    echo "Unable to find full path for script. Please re-try"
+    echo "launching it using a fully rooted path."
+    exit 1
+  fi
+  ;;
+esac
+
+here=`echo $c | sed -e 's+/[^/]*$++'`
+here=`echo $here | sed -e 's+/[^/]*$++'`
+
+find $here/packages \( -name \*.tst -o -name \*.rlg \) -print | xargs $here/scripts/copy-to-xmpl.sh
+
+
+
