2421c476c4ce873332a18bf1a17fcb75efe73dbd
Rainer Schöpf <schoepf@users.sourceforge.net>
2012-02-18 09:30:33 +0000

    Separate configuration files to produce the HTML version of the manual with and without the SourceForge logo.
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@1577 2bfe0521-f11c-4a00-b80e-6202646ff360

--- doc/manual/mkhtml.sh
+++ doc/manual/mkhtml.sh
@@ -4,17 +4,21 @@ JOBNAME=manual-pdf
 
 CONFIG=reduce
 
+test "$1" = "for-sf" && CONFIG=reduce-sf
+
 # Remove .ind file, could be left over from latex run
 rm ${JOBNAME}.ind
 
-mk4ht htlatex ${JOBNAME} reduce,2
+mk4ht htlatex ${JOBNAME} ${CONFIG},3
 
 tex '\def\filename{{'${JOBNAME}'}{idx}{4dx}{ind}} \input idxmake.4ht'
 
 # finally, run makeindex and latex once
 makeindex -o ${JOBNAME}.ind ${JOBNAME}.4dx
 
-mk4ht htlatex ${JOBNAME} reduce,2
+mk4ht htlatex ${JOBNAME} ${CONFIG},3
+
+mv ${JOBNAME}.html index.html
 
 exit 0
 
