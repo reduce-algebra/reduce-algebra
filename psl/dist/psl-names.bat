75a99b3969ba4701fac5c2cce30d9db3c5860109
Rainer Schöpf <schoepf@users.sourceforge.net>
2012-10-16 16:19:35 +0000

    Remove extra whitespace in Windows batch file, which caused incorrect variable settings
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@1774 2bfe0521-f11c-4a00-b80e-6202646ff360

--- psl/dist/psl-names.bat
+++ psl/dist/psl-names.bat
@@ -1,33 +1,33 @@
 @off echo
 
 set proot=%psl%
-set psys=$proot\bin\win32 
+set psys=$proot\bin\win32
 rem # All PSL executables go here.
-set pl=%psl%\dist\lap\win32     
+set pl=%psl%\dist\lap\win32
 rem # Loadable files
 
-set pk=%psl%\dist\kernel	
+set pk=%psl%\dist\kernel
 rem # Machine-independent kernel sources
-set pxk=%psl%\dist\kernel\win32 
+set pxk=%psl%\dist\kernel\win32
 rem # Machine specific kernel sources
 
-set pnk=%psl%dist\\nonkernel	
+set pnk=%psl%dist\\nonkernel
 rem # Machine-independent non-kernel
-set pxnk=%psl%dist\nonkernel\win32  
+set pxnk=%psl%dist\nonkernel\win32
 remi # Machine specific non-kernel
-set pnkl=%psl%\dist\nonkernel\win32\lap  
+set pnkl=%psl%\dist\nonkernel\win32\lap
 rem # Non-Kernel binaries
 
-set pc=%psl%\dist\comp		
+set pc=%psl%\dist\comp
 rem # Machine-independent compiler sources
-set pxc=%psl%\dist\comp\win32	
+set pxc=%psl%\dist\comp\win32
 rem # Machine specific compiler sources
 
-set pu=%psl%\dist\util		
+set pu=%psl%\dist\util
 rem # Machine-independent utility programs
-set pxu=%psl%\dist\util\win32	
+set pxu=%psl%\dist\util\win32
 rem # Machine specific utility program sources
 
-set pdist=%psl%\dist\distrib	
+set pdist=%psl%\dist\distrib
 rem # Distribution support
 
