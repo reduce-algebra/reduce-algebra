b08c7ab243ef0858de8278541701efb0e2d29504
Thomas Sturm <thomas-sturm@users.sourceforge.net>
2013-01-08 07:56:47 +0000

    scripts/pslmem64.sh is a hook for altering the 64 bit psl heapsize on the file system level.
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@1881 2bfe0521-f11c-4a00-b80e-6202646ff360

---
+++ scripts/pslmem64.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Print the desired memory for 64 bit psl-based reduce in (decimal) MB.
+# The default is 1000, which is 1 GB. Note that on many architectures, psl
+# used a stop-and-copy model for gc so that the memory is split into two
+# heaps of which only one is available at any time.
+#
+# For getting information on the memory at runtime, go:
+#
+# load_package redlog;
+# lisp meminfo();
+#
+
+echo 1000
--- scripts/runpsl.sh
+++ scripts/runpsl.sh
@@ -80,7 +80,7 @@ else
       STORE=16000000
       if test -f $here/../pslbuild/$host$hx/psl/64
       then
-        STORE=1000
+        STORE=$($here/pslmem64.sh)
       fi
       bin="$here/../pslbuild/$host$hx/psl/$ap"
 #     rm -f $here/../bin/$scr
