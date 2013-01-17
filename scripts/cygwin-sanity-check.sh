bef0e5b9e3576d835e9c4b3048211b8eda52c53a
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2013-01-17 21:59:28 +0000

    Add a script to check if enough cygwin modules are installed to allow CSL-reduce fetching and building
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@1888 2bfe0521-f11c-4a00-b80e-6202646ff360

---
+++ scripts/cygwin-sanity-check.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# This little script is intended to check if all the cygwin
+# packages that you need in order to build the CSL version of
+# Reduce are present. The list of packages here is short
+# because many others will automatically be selected by the cygwin
+# setup program as prerequisites for these ones. The list here was
+# prepared and checked in January 2013. Changes in cygwin could make
+# it obsolete at some stage, but even then it should help to provide
+# a starting point.
+#
+
+for m in subversion openssh automake make mingw64-i686-gcc-g++ \
+         mingw64-x86_64-gcc-g++ libncurses-devel gcc4-g++ \
+         libXft-devel libX11-devel libXext-devel
+do
+  if cygcheck -c -d $m | grep $m > /dev/null
+  then
+    echo "Good: $m is installed"
+  else
+    echo "To build Reduce you should install $m"
+  fi
+done
+
