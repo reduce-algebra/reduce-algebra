c57302e20d72e0b9c2ef6abc6c7d416548027542
Thomas Sturm <thomas-sturm@users.sourceforge.net>
2013-08-01 14:21:38 +0000

    Build file for regression logs.
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@2168 2bfe0521-f11c-4a00-b80e-6202646ff360

---
+++ packages/redlog/regressions/make.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# I want this script to be one I can launch from anywhere, so
+# to access files etc I need to know where it lives.
+
+a=$0
+c=unknown
+case $a in
+/* )
+  c=$a  
+  ;;
+*/* )
+  case $a in
+  ./* )
+    a=`echo $a | sed -e s+./++`
+    ;;
+  esac
+  c=`pwd`/$a
+  ;;
+* ) 
+  for d in $PATH
+  do
+    if test -x $d/$a
+    then
+      c=$d/$a
+    fi
+  done
+  if test $c = "unknown" ;then
+    echo "Unable to find full path for script. Please re-try"
+    echo "launching it using a fully rooted path."
+    exit 1
+  fi
+  ;;
+esac
+
+here=`echo $c | sed -e 's+/[^/]*$++'`
+here=`echo $here | sed -e 's+/[^/]*$++'`
+here=`echo $here | sed -e 's+/[^/]*$++'`
+here=`echo $here | sed -e 's+/[^/]*$++'`
+
+newrlg=""
+
+for p in *.tst; do
+    base=$(basename $p .tst)
+    rlg=$base.rlg
+    csltime=$base.csltime
+    psltime=$base.psltime
+    if [ "$rlg" -ot "$p" ] || [ "$csltime" -ot "$p" ] || [ "$psltime" -ot "$p" ]; then
+	echo -n "$base: installing ... "
+	$here/generic/redlogtest/rltest-install.sh $base &> /dev/null
+	echo "done"
+	newrlg="$newrlg $rlg"
+    else
+	echo "$rlg: ok"
+    fi
+done
+
+if [ "$newrlg" != "" ]; then
+    echo "Please, check the content of the following newly installed files: $newrlg"
+fi
