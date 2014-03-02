354d45abafe7ad217fcc3aff7e52167a6bdf1d66
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2014-03-02 11:34:54 +0000

    Try to improve autogen.sh
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@2388 2bfe0521-f11c-4a00-b80e-6202646ff360

--- autogen.sh
+++ autogen.sh
@@ -46,154 +46,29 @@ here=`echo $c | sed -e 's+/[^/]*$++'`
 save=`pwd`
 cd $here
 
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo "Updating autoconf scripts in $here"
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo
 
-#
-# I need a short essay to explain what is going on here. I really want to
-# be doubleplus certain that when I run this script it installs versions of
-# all autoconf and automake-related files everywhere. So to be extra careful
-# I will remove old copies first, even if I might hope that "autoreconf -f -i"
-# might overwrite existing copies with new ones.
-# The when the tools search for files such as config.guess they can look
-# in the current directory or its parent or grandparent. To ensure I get
-# unambiguously simple-to-find versions everywhere I run autoreconf on the
-# deepest nested directories first, so that it will install all files.
-# This is perhaps redundant but it reduces (in my opinion) confusion when
-# looking at the trees, even though it leads to the need for complexity and
-# mess here.
-#
-
-rm -f compile config.guess config.sub depcomp install-sh missing
-cd scripts
-rm -f compile config.guess config.sub depcomp install-sh missing
-cd ../csl
-rm -f compile config.guess config.sub depcomp install-sh missing
-cd cslbase
-rm -f compile config.guess config.sub depcomp install-sh missing
-cd crlibm
-rm -f compile config.guess config.sub depcomp install-sh missing
-cd ../../fox
-rm -f compile config.guess config.sub depcomp install-sh missing
-cd ../../psl
-rm -f compile config.guess config.sub depcomp install-sh missing
-cd support-packages/xport-2.05
-rm -f compile config.guess config.sub depcomp install-sh missing
-cd ../../..
-
-cd scripts
-echo " "
-echo "updating in `pwd`"
-if aclocal
-then :
-else
-  echo "reconfiguring failed in $here/scripts"
-  cd $save
-  exit 1
-fi
-if autoconf
-then :
-else
-  echo "reconfiguring failed in $here/scripts"
-  cd $save
-  exit 1
-fi
-
-cd ../csl/cslbase/crlibm
-echo " "
-echo "updating in `pwd`"
-if autoreconf -i -f -v && autoheader --force
-then :
-else
-  echo "autoreconf failed in $here/csl/cslbase/crlibm"
-  cd $save
-  exit 1
-fi
-
-cd ..
-echo " "
-echo "updating in `pwd`"
-if autoreconf -i -f -v && autoheader --force
-then :
-else
-  echo "autoreconf failed in $here/csl/cslbase"
-  cd $save
-  exit 1
-fi
-
-cd ../fox
-echo " "
-echo "updating in `pwd`"
-if autoreconf -i -f -v
-then :
-else
-  echo "autoreconf failed in $here/csl/fox"
-  cd $save
-  exit 1
-fi
-
-if test -d ../foxtests
-then
-  cd ../foxtests
-  echo " "
-  echo "updating in `pwd`"
-  rm -f compile config.guess config.sub depcomp install-sh missing
-  if autoreconf -i -f -v
-  then :
-  else
-    echo "autoreconf failed in $here/csl/foxtests"
-    cd $save
-    exit 1
+for x in `find $here \( -name configure.ac -o -name configure.in \) -print`
+do
+#  echo $x
+  x1=${x%/configure.ac}
+  cd ${x1%/configure.in}
+  echo "============================================================"
+  echo Updating in `pwd`
+  rm -rf aclocal.m4 autom4te.cache
+# The files configure and Makefile.in might sometimes have been
+# hand written... if so I had better not remove them!
+  if test -f configure.ac || test -f configure.in
+  then
+    rm -f configure
   fi
-fi
+  if test -f Makefile.am
+  then
+    rm -f Makefile.in
+  fi
+  autoreconf -f -i
+done
 
-cd ..
-echo " "
-echo "updating in `pwd`"
-if ./autogen.sh
-then :
-else
-  echo "reconfiguring failed in $here/csl"
-  cd $save
-  exit 1
-fi
-
-cd ../psl/support-packages/xport-2.05
-echo " "
-echo "updating in `pwd`"
-if autoreconf -i -f -v
-then :
-else
-  echo "autoreconf failed in $here/psl/support-packages/xport-2.05"
-  cd $save
-  exit 1
-fi
-
-cd ../..
-echo " "
-echo "updating in `pwd`"
-if autoreconf -i -f -v
-then :
-else
-  echo "autoreconf failed in $here/psl"
-  cd $save
-  exit 1
-fi
-
-cd ..
-
-echo " "
-echo "opdating in `pwd`"
-if autoreconf -i -f -v
-then :
-else
-  echo "autoreconf failed in $here"
-  cd $save
-  exit 1
-fi
-
+cd $here
 scripts/resetall.sh
 
 cd $save
