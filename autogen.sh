5bb09c53eead0b15ab8e0a6bef7f093685ed3e11
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2014-03-05 10:17:11 +0000

    Further attempt to make autogen.sh robust, and clean up crlibm configure.ac/Makefile.am to avoid autoconf warnings
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@2389 2bfe0521-f11c-4a00-b80e-6202646ff360

--- autogen.sh
+++ autogen.sh
@@ -47,26 +47,61 @@ save=`pwd`
 cd $here
 
 
-for x in `find $here \( -name configure.ac -o -name configure.in \) -print`
+for x in `find $here -name configure.ac -print`
 do
-#  echo $x
-  x1=${x%/configure.ac}
-  cd ${x1%/configure.in}
   echo "============================================================"
-  echo Updating in `pwd`
-  rm -rf aclocal.m4 autom4te.cache
+  case $x in
+# I will not try reconfiguring in the wxWidgets directory. I will
+# want the files there to be exactly as unpacked from the wxWidgets
+# archive.
+  */csl/wxWidgets*)
+    echo Leaving $x untouched.
+    ;;
+  *)
+    x1=`echo $x | sed -e 's+/configure.ac$++'`
+    cd $x1
+    echo Updating autoconf files in `pwd`:
+    rm -rf aclocal.m4 autom4te.cache
 # The files configure and Makefile.in might sometimes have been
 # hand written... if so I had better not remove them!
-  if test -f configure.ac || test -f configure.in
-  then
-    rm -f configure
-  fi
-  if test -f Makefile.am
-  then
-    rm -f Makefile.in
-  fi
-  autoreconf -f -i
+    if test -f configure.ac || test -f configure.in
+    then
+      rm -f configure
+    fi
+    if test -f Makefile.am
+    then
+      rm -f Makefile.in
+    fi
+    autoreconf -f -i
+  esac
 done
+
+# Now just to be double plus certain that all is well I will
+# reset date-stamps.
+
+echo Now reset all date-stamps...
+echo Step 1 of 5: configure.ac and configure.in.
+find . \( -name configure.ac -o -name configure.in \
+          -o -name Makefile.am \) -print | xargs touch
+sleep 1
+
+echo Step 2 of 5: aclocal.m4:
+find . -name aclocal.m4 -print | xargs touch
+sleep 1
+
+echo Step 3 of 5: config.h.in:
+find . -name config.h.in -print | xargs touch
+sleep 1
+
+echo Step 4 of 5: Makefile.in:
+find . -name Makefile.in -print | xargs touch
+sleep 1
+
+echo Step 5 of 5: configure:
+find . -name configure -print | xargs touch
+find . -name configure -print | xargs chmod +x
+
+echo Date-stamps should now be in the proper sequence.
 
 cd $here
 scripts/resetall.sh
