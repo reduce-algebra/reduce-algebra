94c436c3d89aaf15e7ee5a82e7f182f17980fb6e
Rainer Schöpf <schoepf@users.sourceforge.net>
2014-10-01 19:34:11 +0000

    more updates for inclusion of contribute package docs
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@2711 2bfe0521-f11c-4a00-b80e-6202646ff360

--- doc/manual/mkhtml.sh
+++ doc/manual/mkhtml.sh
@@ -4,19 +4,20 @@ JOBNAME=manual
 
 CONFIG=reduce
 
+SECTIONINGDEPTH=3
 test "$1" = "for-sf" && CONFIG=reduce-sf
 
 # Remove .ind file, could be left over from latex run
 rm -f ${JOBNAME}.ind
 
-mk4ht htlatex ${JOBNAME} ${CONFIG},3
+mk4ht htlatex ${JOBNAME} ${CONFIG},${SECTIONINGDEPTH}
 
 tex '\def\filename{{'${JOBNAME}'}{idx}{4dx}{ind}} \input idxmake.4ht'
 
 # finally, run makeindex and latex once
 makeindex -o ${JOBNAME}.ind ${JOBNAME}.4dx
 
-mk4ht htlatex ${JOBNAME} ${CONFIG},3
+mk4ht htlatex ${JOBNAME} ${CONFIG},${SECTIONINGDEPTH}
 
 cp ${JOBNAME}.html index.html
 
