8793acc7ba3ca33073468b66d70c9f8ddd97f243
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2014-10-13 07:46:19 +0000

    make detection of 64-bit cygwin more robust
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@2745 2bfe0521-f11c-4a00-b80e-6202646ff360

--- winbuild/make-cyg64.sh
+++ winbuild/make-cyg64.sh
@@ -27,30 +27,8 @@ where=""
 for x in `find /cygdrive/*/*/bin -name cygwin1.dll -printf "%h\\n" 2>/dev/null`
 do
   printf "... testing $x/cygwin1.dll\n"
-  case `$objdump -a $x/cygwin1.dll 2>/dev/null`
-  in
-  *pei-i386*)
-     if test "x$w" = "x32"
-     then
-       printf "Found $x\n"
-       where="$x"
-     fi
-     ;;
-  *pei-x86-64*)
-     if test "x$w" = "x64"
-     then
-       printf "Found $x\n"
-       where="$x"
-     fi
-     ;;
-  esac
-done
-if test "x$where" = "x"
-then
-  printf "First pass failed to find cygwin: will now scan whole file-system\n"
-  for x in `find /cygdrive/* -name cygwin1.dll -printf "%h\\n" 2>/dev/null`
-  do
-    printf "... testing $x/cygwin1.dll\n"
+  if test -x $x/bash.exe
+  then
     case `$objdump -a $x/cygwin1.dll 2>/dev/null`
     in
     *pei-i386*)
@@ -68,17 +46,43 @@ then
        fi
        ;;
     esac
+  fi
+done
+if test "x$where" = "x"
+then
+  printf "First pass failed to find cygwin: will now scan whole file-system\n"
+  for x in `find /cygdrive/* -name cygwin1.dll -printf "%h\\n" 2>/dev/null`
+  do
+    printf "... testing $x/cygwin1.dll\n"
+    if test -x $x/bash.exe
+    then
+      case `$objdump -a $x/cygwin1.dll 2>/dev/null`
+      in
+      *pei-i386*)
+         if test "x$w" = "x32"
+         then
+           printf "Found $x\n"
+           where="$x"
+         fi
+         ;;
+      *pei-x86-64*)
+         if test "x$w" = "x64"
+         then
+           printf "Found $x\n"
+           where="$x"
+         fi
+         ;;
+      esac
+    fi
   done
 fi
 if test "x$where" = "x"
 then
   printf "Unable to find a suitable $w bit version of cygwin1.dll"
-  exit 1
+else
+  where=`cygpath -m ${where%/bin}`
+  i686-w64-mingw32-gcc other-cygwin.c -Dcygwin=$where -o cyg$w
+  printf "cyg$w.exe created to run $w bit cygwin tasks\n"
 fi
-where=`cygpath -m ${where%/bin}`
-i686-w64-mingw32-gcc other-cygwin.c -Dcygwin=$where -o cyg$w
-
-printf "cyg$w.exe created to run $w bit cygwin tasks\n"
-
 done
 
