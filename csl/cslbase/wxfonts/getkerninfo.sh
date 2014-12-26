776d859e9e88776391cd36468648af7ea044db91
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2014-12-26 09:09:38 +0000

    Now use fontforge to extract metrics from wxfonts
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@2882 2bfe0521-f11c-4a00-b80e-6202646ff360

--- csl/cslbase/wxfonts/getkerninfo.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-for x in *.otf fireflysung.ttf
-do
-  echo ot2kpx $x
-  ot2kpx $x > tfm/${x%.otf}.kpx
-done
-
-
--- csl/cslbase/wxfonts/getmetrics.sh
+++ csl/cslbase/wxfonts/getmetrics.sh
@@ -1,10 +1,25 @@
-#! /bin/bash
+# !/bin/sh
 
-for x in *.ttf
+# Create .afm file of all my fonts,
+
+# When I am running on my own Windows machine I have fontforge installed
+# in the location shown below. On Linux (etc) I want it to be found in
+# the standard way.
+if which fontforge > /dev/null
+then
+  ff=fontforge
+else
+  ff="/c/Program Files (x86)/FontForgeBuilds/fontforge.bat"
+fi
+printf "Using %s for fontforge\n" $ff
+
+for i in STIX*.otf cmuntt.otf fireflysung.ttf
 do
-  echo ttf2ufm -a -F $x ${x%.ttf}
-  ttf2ufm -a -F $x ${x%.ttf}
+  "$ff" -script toafm.script $i
 done
 
-mv *.ufm tfm
+# I do not really want the ".ps" files - just the .afm ones
+rm *.ps
+mv *.afm tfm
 
+printf "Conversion complete\n"
