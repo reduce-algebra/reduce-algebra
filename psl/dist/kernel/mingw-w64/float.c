30af558bdb4fa0f754b58be9cf5056a05b09c716
Rainer Schöpf <schoepf@users.sourceforge.net>
2015-04-19 16:08:32 +0000

    PSL for Windows 64:  print NaN and +/-inf correctly
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@3086 2bfe0521-f11c-4a00-b80e-6202646ff360

--- psl/dist/kernel/mingw-w64/float.c
+++ psl/dist/kernel/mingw-w64/float.c
@@ -181,25 +181,28 @@ uxwritefloat(buf, flt, convstr)
 
   sprintf(temps,convstr, *flt);
 
-  /* Make sure that there is a trailing .0
-   */
-  dot = strrchr(temps, '.');
-  if (dot == '\0')
-    /* Check to see if the number is in scientific notation. If so, we need
-     *  add the .0 into the middle of the string, just before the e.
-     */
-    if ((e = strrchr(temps, 'e')) || (e = strrchr(temps, 'E')))
-      {
-	strcpy(tempbuf, e);       /* save save exponent part */
-	*e = '\0'; 
-	strcat(temps, ".0");     /* Add .0 ono original string */
-	strcat(temps, tempbuf);  /* add the exponent part onto the end */
-      }
-  else
+  if (finite(*flt))
     {
-      strcat(temps, ".0");
+
+    /* Make sure that there is a trailing .0
+     */
+    dot = strrchr(temps, '.');
+    if (dot == '\0')
+      /* Check to see if the number is in scientific notation. If so, we need
+       *  add the .0 into the middle of the string, just before the e.
+       */
+      if ((e = strrchr(temps, 'e')) || (e = strrchr(temps, 'E')))
+        {
+	  strcpy(tempbuf, e);       /* save save exponent part */
+	  *e = '\0'; 
+	  strcat(temps, ".0");     /* Add .0 ono original string */
+	  strcat(temps, tempbuf);  /* add the exponent part onto the end */
+        }
+      else
+        {
+          strcat(temps, ".0");
+        }
     }
-  
   /* Install the length of the string into the Lisp header word
    */
   *((long long *)buf) = strlen(temps) - 1;
