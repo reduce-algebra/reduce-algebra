1260a6ac0fe8033ce154e3a248f0826f1f4fbfcd
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2016-01-24 23:20:42 +0000

    Small further fix re windows snapshots - which I now need to test.
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@3436 2bfe0521-f11c-4a00-b80e-6202646ff360

--- winbuild/cslcyg32x.sh
+++ winbuild/cslcyg32x.sh
@@ -1,13 +1,13 @@
 #! /bin/bash -v
 
 # Configure and build CSL version from scratch. This makes a 32-bit cygwin
-# version, but will be launched from a cygwin64 shell. Note it needs my "cyg32"
-# utility to arrange that building happens under 32-bit cygwin...
+# version, but will be launched from a cygwin64 shell. Note it needs my
+# "cygalt" utility to arrange that building happens under 32-bit cygwin...
 
 
 # For this to work you MUST have a 32-bit version of cygwin installed.
 
-reduce=`./cyg32 cygpath -a ..`
+reduce=`./cygalt cygpath -a ..`
 reduce="${reduce%/}"
 echo $reduce
 
@@ -17,9 +17,9 @@ pushd cslcyg32
 
 mkdir crlibm
 pushd crlibm
-../../cyg32 $reduce/csl/cslbase/crlibm/configure \
+../../cygalt $reduce/csl/cslbase/crlibm/configure \
     --prefix=$reduce/winbuild/cslcyg32
-../../cyg32 make install
+../../cygalt make install
 popd
 
 mkdir fox
@@ -28,17 +28,17 @@ foxflags="--enable-release --with-opengl=no \
           --disable-jpeg --disable-zlib --disable-bz2lib \
           --disable-png --disable-tiff"
 extras="--with-xft --with-xim"
-../../cyg32 $reduce/csl/fox/configure \
+../../cygalt $reduce/csl/fox/configure \
     --prefix=$reduce/winbuild/cslcyg32 $foxflags $extras
-../../cyg32 make install
+../../cygalt make install
 popd
 
 mkdir csl
 pushd csl
-../../cyg32 $reduce/csl/cslbase/configure --prefix=$reduce/winbuild/cslcyg32 \
+../../cygalt $reduce/csl/cslbase/configure --prefix=$reduce/winbuild/cslcyg32 \
     --with-cygwin --with-fox=$reduce/winbuild/cslcyg32 --with-fox-pending \
     --without-wx
-../../cyg32 make
+../../cygalt make
 ls -lh reduce.exe reduce.img csl.exe csl.img
 popd
 
