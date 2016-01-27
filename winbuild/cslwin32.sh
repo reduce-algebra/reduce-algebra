c9fc37333e4670dad2c5e8fe3beaefef04c8f5c9
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2016-01-27 15:02:50 +0000

    Further work on especially the Windows spanshop build process
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@3437 2bfe0521-f11c-4a00-b80e-6202646ff360

--- winbuild/cslwin32.sh
+++ winbuild/cslwin32.sh
@@ -17,11 +17,7 @@ mkdir crlibm
 pushd crlibm
 $reduce/csl/cslbase/crlibm/configure --prefix=$here/cslwin32 \
     --host=i686-w64-mingw32
-# This script is invoked on its own ahead of the other builds of Reduce
-# versions and so I feel entitled to use "-j4" for parallel building.
-# I argue that even on a single processor machine (now increasingly rare)
-# this will not be a calamity, and on a multicore machine it will really help.
-make -j4 install
+make install
 popd
 
 mkdir fox
@@ -31,7 +27,7 @@ foxflags="--enable-release --with-opengl=no \
           --disable-png --disable-tiff"
 $reduce/csl/fox/configure --prefix=$here/cslwin32 $foxflags \
     --host=i686-w64-mingw32
-make -j4 install
+make install
 popd
 
 mkdir csl
@@ -39,11 +35,12 @@ pushd csl
 $reduce/csl/cslbase/configure --prefix=$here/cslwin32 \
     --host=i686-w64-mingw32 --with-fox=$here/cslwin32 \
     --with-fox-pending --without-wx
-make -j4 standard-c-code
-make -j4
+make standard-c-code
+make
 ls -lh reduce.exe reduce.com reduce.img csl.exe csl.img
 popd
 
 popd
 
 # end of script
+ 
