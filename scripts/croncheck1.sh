34229e01f23f4e1bf6b8b3ecebff5f5fd58ba37b
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2016-04-01 16:06:48 +0000

    croncheck1.sh that builds things on a Mac and two VMs
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@3536 2bfe0521-f11c-4a00-b80e-6202646ff360

--- scripts/croncheck1.sh
+++ scripts/croncheck1.sh
@@ -1,18 +1,37 @@
 #! /bin/bash -v
 
-# WINDOWS_VM is the name of a virtual box VM that will run Windows
+# WINDOWS_VM is the name of a virtual box VM that will run Windows.
 WINDOWS_VM=windows
 # WINDOWS_USER can be an empty string if the user-name that will be
 # used on the VM matches that used on the host. Otherwise it can be set
-# as in WINDOWS_USER="acn1@" with a name and a "@" sign and that will be used
-# with ssh to access the virtual machine
+# as in WINDOWS_USER="acn1" with a name that will be used with ssh to
+# access the virtual machine. If none is given then the current user
+# (as obtained by the "whoami" command) will be used, and in many cases
+# I hope that this default will be good enough!
 WINDOWS_USER=
 # WINDOWS_PORT is the port number that has been forwarded to the VM.
+# This needs to be set up when networking is set up for the virtualbox
+# VM. It should use NAT networking and there is an "advanced" option for
+# port forwarding that should forward this host port to port 22 on the
+# guest.
 WINDOWS_PORT=2201
 
 LINUX_VM=linux
 LINUX_USER=
 LINUX_PORT=2202
+
+# I *HOPE* that while the above entries may need customising or
+
+
+if test "x$WINDOWS_USER" = "x"
+then
+  WINDOWS_USER=`whoami`
+fi
+
+if test "x$LINUX_USER" = "x"
+then
+  LINUX_USER=`whoami`
+fi
 
 # Keep a backup of the previous snapshots and start with an empty
 # directory for the new ones I am about to create.
@@ -29,31 +48,25 @@ fi
 
 # I need the following so I grab the correct version of svn, as installed via
 # macports! An in general to be certain that I can use macports things.
+
 export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
 
 mkdir snapshots
 
 # Ensure that my copy of Reduce is fully up to date.
-cd reduce-distribution
+pushd reduce-distribution
 svn -R revert .
 svn update
 
-# Build a snapshot for this machine - a Macintosh
+# Unpack as for a local build
 pushd macbuild
-make
-cp Redu*.dmg $HOME/snapshots
+rm -rf C
+make clean
+make C.stamp
 popd
 
 # Next launch a VM to regenerate a Linux distribution
 vboxmanage startvm $LINUX_VM -type headless
-
-# I will unpack the Reduce files while waiting for the machine to start,
-# and unpacking on the Mac will be faster and safer than doing so in theVM.
-
-pushd debianbuild
-make clean
-make C.stamp
-popd
 
 # Wait until virtualbox is running. I do this by sending ssh requests
 # repeatedly until ons is responded to. I will try up to 20 at 30-second
@@ -66,6 +79,7 @@ do
   then
     break
   fi
+  printf "Sleeping to allow Linux VM to start up...\n"
   sleep 30
 done
 
@@ -77,26 +91,49 @@ done
 # like
 #   <username> ALL=(ALL) NOPASSWD:ALL
 # in /etc/sudoers
+
+# First I will clear away any previous version on the VM and copy across
+# files from the host.
+
+ssh -p $LINUX_PORT ${LINUX_USER}localhost rm -rf debianbuild
+scp -r -P $LINUX_PORT debianbuild ${LINUX_USER}localhost:.
+
+# This next line copies all the files across using tar to pack and unpack so
+# that there is only a single ssh transaction. Whether this is really better
+# or worse than using "scp -r" is not clear to me. I use only basic facilities
+# of "tar" in part because the default Mac one is a BSD tar not a GNU version.
+
+pushd macbuild
+tar cfz - C | \
+  ssh -p $LINUX_PORT ${LINUX_USER}localhost \
+    \( cd debianbuild \; tar xfz - \)
+popd
+
+# Create the file that marks the source files as present and up to date
+
+ssh -p $LINUX_PORT ${LINUX_USER}localhost touch debianbuild/C.stamp
+
+# Now do the main build of all the Linux binaries and packages.
+
 ssh -p $LINUX_PORT ${LINUX_USER}localhost \( \
-  cd /media/sf_reduce-distribution/debianbuild \; \
-  time make \; \
-  sudo /sbin/shutdown -h now \)
+  cd debianbuild \; \
+  time make \)
 
-cp debianbuild/*.deb debianbuild/*.rpm $HOME/snapshots
+# Recover the built files.
+scp -P $LINUX_PORT ${LINUX_USER}localhost:debianbuild/\*.deb $HOME/snapshots
+scp -P $LINUX_PORT ${LINUX_USER}localhost:debianbuild/\*.rpm $HOME/snapshots
+
+# Shut down the guest. I do this by issing a command within the VM rather
+# than by an externally forced power-down since I hope that will count
+# as kinder. But done this way means that the account used on the VM has
+# to have "sudo" rights without neeting to quote a password.
+
+ssh -p $LINUX_PORT ${LINUX_USER}localhost sudo /sbin/shutdown -h now
 
 
-# I will fill this in in a while...
 
 # Start a Windows build. This will take a long time!
 vboxmanage startvm $WINDOWS_MC -type headless
-
-# Unpack files on the host not in the VM for speed and to avoid trouble
-# with symbolic links.
-
-pushd winbuild
-make clean
-make C.stamp
-popd
 
 # Wait until virtualbox is running. I do this by sending ssh requests
 # repeatedly until ons is responded to. I will try up to 40 at 30-second
@@ -111,16 +148,54 @@ do
   then
     break
   fi
+  printf "Sleeping to allow Windows VM to start up...\n"
   sleep 30
 done
 
-ssh -p $WINDOWS_PORT ${WINDOWS_USER}localhost \( \
-  cd /cygdrive/e/winbuild \; \
-  time make \; \
-  sleep 60 \; \
-  shutdown /p \)
+# Now in a way that is just as was done for Linux I will put the
+# necessary files on the Windows guest VM
 
-cp winbuild/Output/Reduce*.exe $HOME/snapshots
+ssh -p $WINDOWS_PORT ${WINDOWS_USER}localhost rm -rf winbuild
+scp -r -P $WINDOWS_PORT winbuild ${WINDOWS_USER}localhost:.
+pushd macbuild
+tar cfz - C | \
+  ssh -p $WINDOWS_PORT ${WINDOWS_USER}localhost \
+    \( cd winbuild \; tar xfz - \)
+popd
+ssh -p $WINDOWS_PORT ${WINDOWS_USER}localhost touch winbuild/C.stamp
+
+# That ha snow established the winbuild directory...
+
+# Now do the main build of all the Windows binaries and packages.
+
+# Note that the following step builds 6 copies of Reduce and takes
+# several hours to complete. The versions built are
+#   csl-win32, csl-win64, csl-cygwin32, csl-cygwin64, psl-win32 and psl-win64
+
+ssh -p $WINDOWS_PORT ${WINDOWS_USER}localhost \( \
+  cd winbuild \; \
+  time make \)
+
+# Recover the built files.
+scp -P $WINDOWS_PORT ${WINDOWS_USER}localhost:winbuild/\*.exe $HOME/snapshots
+
+# Shut down the guest. I do this by issing a command within the VM rather
+# than by an externally forced power-down since I hope that will count
+# as kinder. But done this way means that the account used on the VM has
+# to have "sudo" rights without neeting to quote a password.
+
+ssh -p $WINDOWS_PORT ${WINDOWS_USER}localhost shutdown /p
+
+
+# Now make the Mac version. I wanted the archive unpacked early since that
+# is what I copy for use on the other platforms, and doing things that way
+# means I know that all systems build from exactly the same version of
+# the sources.
+
+pushd macbuild
+make
+cp Reduce*.dmg $HOME/snapshots/Reduce-snapshot_`date "+%Y%m%d"`.dmg
+popd
 
 popd
 printf "Can now copy these files to sourceforge:\n"
