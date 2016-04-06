281d54d00f3fad11b5fffd7fc8c8f8ee7a29d2be
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2016-04-06 07:51:55 +0000

    Try to put set-up-snapshot-builder so it will be more generic.
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@3563 2bfe0521-f11c-4a00-b80e-6202646ff360

--- scripts/croncheck.sh
+++ scripts/croncheck.sh
@@ -3,11 +3,15 @@
 # is switched on. It will arrange that about once per week it will
 # regenerate snapshots of Reduce. See crontab.entry
 
-cd $HOME
+# I want this script to be one I can launch from anywhere, but at least
+# some of its sub-scripts will not be so generous. So find out where it
+# lives so that other locations can be found relative to that.
+here="$0";while test -L "$here";do here=`ls -ld "$here" | sed 's/.*-> //'`;done
+here=`cd \`dirname "$here"\` ; pwd -P`
 
 # Find what the time is now and when the next snapshot is due...
 
-req=`cat croncheck.dat 2>/dev/null`
+req=`cat $here/croncheck.dat 2>/dev/null`
 now=`date +%s`
 if test "x$req" = "x"
 then
@@ -26,22 +30,22 @@ then
   left=$((($req - $now) / 3600))
 # I will keep an old version of the logf, but then preserve just the last 20
 # lines from it.
-  mv croncheck.log croncheck.log.old
-  tail -20 croncheck.log.old > croncheck.log
-  printf "Time %s (%d hours to go)\n" "`date`" $left >> croncheck.log
+  mv $here/croncheck.log $here/croncheck.log.old
+  tail -20 $here/croncheck.log.old > $here/croncheck.log
+  printf "Time %s (%d hours to go)\n" "`date`" $left >> $here/croncheck.log
 # printf "Nothing needs doing right now (%s:%s:%s)\n" `whoami` `pwd` $USER >> croncheck.log
   exit 0
 fi
-printf "Time %s run croncheck task (%s:%s:%s)\n" "`date`" `whoami` `pwd` $USER >> croncheck.log
+printf "Time %s run croncheck task (%s:%s:%s)\n" "`date`" `whoami` `pwd` $USER >> $here/croncheck.log
 
 # Arrange that the next attempt to build will not happen for another week.
 next=$(($now + 600000))
-printf "$next" > croncheck.dat
+printf "$next" > $here/croncheck.dat
 
 # I do the bulk of the work in a separate file croncheck1.sh and keep
 # a log of all the output it creates. Note that this is run on a Macintosh
 # hence the exact order of arguments passed to the "script" command.
 
-script croncheck1.log bash -v ./croncheck1.sh
+script $here/croncheck1.log bash -v $here/croncheck1.sh
 
 exit 0
--- scripts/set-up-snapshot-builder.sh
+++ scripts/set-up-snapshot-builder.sh
@@ -1,22 +1,22 @@
 #! /bin/sh
 
 # This script does some of the work of setting up a Macintosh to be
-# ready to create regulat snapshots of Reduce. It ararnges that a
-# copy of Reduce is checked out in $HOME/reduce-distribution (that is
-# not where I normally put it because I want this to be a copy that
-# is checked out without a password and I want it to be one that I will
-# never build or edit other than via the scripts managed here).
+# ready to create regular snapshots of Reduce.
+# It assumes that the checkout of Reduce that it is within is either one
+# checked out using "svn:" or if it is "ssh+svn:" style that the relevant
+# password is saved so that "svn update" will not ask for a password or
+# any other information.
 
-if ! test -d $HOME/reduce-distribution
-then
-  svn co svn://svn.code.sf.net/p/reduce-algebra/code/trunk reduce-distribution
-fi
+here="$0";while test -L "$here";do here=`ls -ld "$here" | sed 's/.*-> //'`;done
+here=`cd \`dirname "$here"\` ; pwd -P`
 
-# I will REQUIRE three vmware virtual machines, "windows", "linux" and
-# "linux32".
+# I will REQUIRE three vmware virtual machines, "REDUCE-pkg-factory-Windows",
+# "REDUCE-pkg-factory-Ubuntu" and "REDUCE-pkg-factory-Ubuntu32".
 # They must be set up using NAT networking with the Windows one forwarding
-# local port 2201 to its port 22, linux one forwarding port 2202 and linux32
-# port 2203.
+# local port 2201 to its port 22, 64-bit linux one forwarding port 2202 and
+# 32-bit linux on port 2203. That gets set up using the "advanced" option
+# within the virtualbox network setup when you select NAT networking, and
+# you just add an entry that specifies host port (eg) 2201 and guest port 22.
 
 # Each virtual machine must have a proper set of development tools suitable
 # for building Reduce, plus Latex for building the manual and "inno setup" on
@@ -24,13 +24,20 @@ fi
 # debianscripts, alien, rpm). For the Linux systems the commands in
 # scripts/ubuntu-sanity-check.sh may help. For Windows you can start with
 # scripts/cygwin-sanity-check.sh but to build documentation you need
-# latex and some other packages.
+# latex and some other packages: the easiest way to get the Windows setup
+# complete would be to install; all the "publishing" modules that cygwin
+# offers.
 #
 # The Mac host must have client SSH keys, while the virtual machines must
 # each run an SSH server and must have .ssh/authorized_keys including the
 # client public keys from the Macintosh host. These client keys must not
 # use a pass-phrase, so that ssh from the macintosh to the VM should happen
 # without need for any extra authentication or interaction.
+# Eg on the host
+#    ssh-keygen -t ecdsa
+# and give an empty passphrase and save the key in the regular place. Then
+# something like
+#    scp -p 2201 .ssh/id_ecdsa.pub localhost:.ssh/authorized_keys
 #
 # If all that is in place then the Macintosh host should be able to use
 #    vboxmanage startvm windows -type headless
@@ -39,20 +46,12 @@ fi
 # and get confirmation that the machine works (once it has had time to start
 # up)
 
-here="$0";while test -L "$here";do here=`ls -ld "$here" | sed 's/.*-> //'`;done
-here=`pwd -P`
+# The file scripts/crontab.entry is a template and "@@@" has to be replaced
+# by the name of the directory that the script will be in. This should do the
+# trick.
 
-crontab $here/crontab.entry
+sed -e "s+@@@+$here+" < $here/crontab.entry | crontab
 
-rm -f $HOME/croncheck.sh
-rm -f $HOME/croncheck1.sh
-rm -f $HOME/croncheck.dat
-rm -f $HOME/make-now.sh
-
-ln -s $here/croncheck.sh $HOME
-ln -s $here/croncheck1.sh $HOME
-ln -s $here/croncheck.dat $HOME
-ln -s $here/make-now.sh $HOME
 
 # end of script
 
