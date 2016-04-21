cf9cf3b23c91147015680762638cc9a67c61effa
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2016-04-21 20:47:34 +0000

    Mostly an attempt to make stuff build on cygwin
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@3614 2bfe0521-f11c-4a00-b80e-6202646ff360

--- libraries/libedit-20140620-3.1/src/editline/readline.h
+++ libraries/libedit-20140620-3.1/src/editline/readline.h
@@ -75,7 +75,7 @@ typedef KEYMAP_ENTRY *Keymap;
 
 #ifndef CTRL
 #include <sys/ioctl.h>
-#if !defined(__sun) && !defined(__hpux) && !defined(_AIX)
+#if !defined(__sun) && !defined(__hpux) && !defined(_AIX) && !defined(__CYGWIN__)
 #include <sys/ttydefaults.h>
 #endif
 #ifndef CTRL
