a3603239a02517d2fea217f1671577a7742527ac
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2016-07-21 14:16:52 +0000

    Fix up the mistake I made in the previous attempt to correct this!
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@3754 2bfe0521-f11c-4a00-b80e-6202646ff360

--- csl/cslbase/wxfwin.cpp
+++ csl/cslbase/wxfwin.cpp
@@ -339,7 +339,6 @@ void add_custom_fonts()
 #elif defined MACINTOSH
 // Note that on a Mac I put the required fonts in the Application Bundle,
 // and so I do not need to take run-time action to make them available.
-#else
 #elif defined UNIX
     FcConfig *config = FcConfigGetCurrent();
     if (config == NULL) config = FcConfigCreate();
@@ -355,6 +354,7 @@ void add_custom_fonts()
     }
     FcConfigSetCurrent(config);
     printf("Activated\n"); fflush(stdout);
+#else
 #error Unknown platform so private fonts not supported.
 #endif
 }
