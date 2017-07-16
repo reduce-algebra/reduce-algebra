87cb65f06bb99364c109ec347aade88698e90680
Rainer Schöpf <schoepf@users.sourceforge.net>
2017-07-16 13:24:57 +0000

    PSL for macintel64: set default heapsize in bpsl executable to 1000MB (which is the default in all scripts anyway).
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@4109 2bfe0521-f11c-4a00-b80e-6202646ff360

--- psl/dist/kernel/macintel64/bpsheap.c
+++ psl/dist/kernel/macintel64/bpsheap.c
@@ -77,10 +77,10 @@ void * sbrk(int);
 
 #define NUMBEROFHEAPS 2
 
-#define MINSIZE        50000000 /* Default total in number of bytes. */
-#define MALLOCSIZE     500000   /* Default size for OS support functions. */
-#define EXTRABPSSIZE   300000   /* Minimum amount to increase bps by. */
-#define MINIMUMHEAPADD 20000    /* Minimum amount to increase heap by */
+#define MINSIZE        1000 * 1024 * 1024  /* Default total in number of bytes. */
+#define MALLOCSIZE     500000              /* Default size for OS support functions. */
+#define EXTRABPSSIZE   300000              /* Minimum amount to increase bps by. */
+#define MINIMUMHEAPADD 20000               /* Minimum amount to increase heap by */
 
 
 #ifndef BPSSIZE
