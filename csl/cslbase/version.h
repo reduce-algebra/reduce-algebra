f77a9df74b430d1ec623142193351b54b7f28d02
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2017-07-17 21:23:38 +0000

    Remove the cslbase/mr directory since I have put the sort of thing it contained elsewhere on sourceforge as part of the process of creating isprime.cpp.
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@4116 2bfe0521-f11c-4a00-b80e-6202646ff360

--- csl/cslbase/version.h
+++ csl/cslbase/version.h
@@ -57,7 +57,7 @@
 // As of February 2016 I am making the file scripts/commit.sh update the
 // revision number stored here...
 
-#define REVISION 4099
+#define REVISION 4107
 
 #endif // header_version_h
 
