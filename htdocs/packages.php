8517dd89c12f2b19633225103c505b02335846f2
Francis Wright <fjwright@users.sourceforge.net>
2017-09-02 14:30:40 +0000

    Website: revised and renamed the Available Packages page to Related Packages.
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@4182 2bfe0521-f11c-4a00-b80e-6202646ff360

--- htdocs/packages.php
+++ htdocs/packages.php
@@ -2,7 +2,7 @@
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta charset="utf-8" />
-	<title>Available REDUCE Packages</title>
+	<title>Related REDUCE Packages</title>
 	<link rel="icon" type="image/png" href="images/icon.png" />
 	<link rel="stylesheet" href="StyleSheet.css" />
 	<style type="text/css">
@@ -13,7 +13,7 @@
 </head>
 <body>
 	<div id="content">
-		<header>Available Packages</header>
+		<header>Related Packages</header>
 		<div id="middle">
 			<nav>
 				<script language="php">
@@ -21,29 +21,26 @@
 				</script>
 			</nav>
 			<div id="main">
-				<p>REDUCE includes a wide variety of specialized packages that are included with the standard release and <a href="documentation.php">documented</a> online. Some additional packages that use REDUCE are also available directly from their authors. These include:</p>
+				<p>REDUCE distributions include a wide variety of specialized packages as <a href="documentation.php">described on the documentation page</a>. The related packages listed below use REDUCE but are not included in REDUCE distributions.</p>
+				<h2>Packages available separately for download</h2>
 				<dl>
-					<dt><a href="http://www.latrobe.edu.au/mathematics-and-statistics/research/geometric-and-algebraic-techniques-for-differential-equations/dimsym">Dimsym</a></dt>
-					<dd>Symmetry Determination and Linear Differential Equation Package<br />
-						Author: James Sherring</dd>
-					<dt><a href="grg32/grg32.php">GRG 3.2</a></dt>
-					<dd>A package for making calculation in differential geometry and field theory as simple and natural as possible<br />
-						Author: Vadim V. Zhytnikov</dd>
-					<dt><a href="http://www.utsc.utoronto.ca/~harper/redten.html">REDTEN</a></dt>
-					<dd>Tensor manipulation package<br />
-						Authors: <a href="mailto:harper@utsc.utoronto.ca">John Harper</a> and <a href="mailto:dyer@utsc.utoronto.ca">Charles Dyer</a></dd>
+					<dt><a href="http://www.latrobe.edu.au/mathematics-and-statistics/research/geometric-and-algebraic-techniques-for-differential-equations/dimsym">Dimsym</a> &ndash; Symmetry Determination and Linear Differential Equation Package</dt>
+					<dd>Authors: James Sherring, Geoff Prince and Michael Jerie</dd>
+					<dt><a href="grg32/grg32.php">GRG 3.2</a> &ndash; for making calculation in differential geometry and field theory as simple and natural as possible</dt>
+					<dd>Author: Vadim V. Zhytnikov</dd>
+					<dt><a href="http://www.utsc.utoronto.ca/~harper/redten.html">REDTEN</a> &ndash; for tensor manipulation</dt>
+					<dd>Authors: <a href="mailto:harper@utsc.utoronto.ca">John Harper</a> and <a href="mailto:dyer@utsc.utoronto.ca">Charles Dyer</a></dd>
 				</dl>
-				<p>Some packages may also be accessed by sending inputs to the authors&#39; servers, namely:</p>
+				<h2>Packages that can only be used online and/or via email</h2>
 				<dl>
-					<dt><a href="http://convode.physique.fundp.ac.be/convode/Main.py?m=0&amp;r=0&amp;i=0&amp;lg=en">CONVODE</a></dt>
-					<dd>Package for solving various systems of differential equations<br />
-						Author: <a href="mailto:alain.moussiaux@unamur.be">Alain Moussiaux</a></dd>
+					<dt><a href="http://convode.physique.fundp.ac.be/convode/Main.py?m=0&amp;r=0&amp;i=0&amp;lg=en">CONVODE</a> &ndash; for solving various systems of differential equations</dt>
+					<dd>Author: <a href="mailto:alain.moussiaux@unamur.be">Alain Moussiaux</a></dd>
 					<dt><a href="http://www.maths.adelaide.edu.au/anthony.roberts/holistic1.php">Holistic discretisation of a dynamical Partial Differential Equation</a><br />
 						<a href="http://www.maths.adelaide.edu.au/anthony.roberts/holistic3.php">Holistic discretisation of three coupled dynamical Partial Differential Equations</a><br />
 						<a href="http://www.maths.adelaide.edu.au/anthony.roberts/sdenf.php">Normal form of stochastic or deterministic multiscale differential equations</a><br />
 						<a href="http://www.maths.adelaide.edu.au/anthony.roberts/sdesm.php">Slow manifold of stochastic or deterministic multiscale differential equations</a><br />
 						<a href="http://www.maths.adelaide.edu.au/anthony.roberts/gencm.php">Construct centre manifolds of ordinary or delay differential equations (autonomous)</a></dt>
-					<dd>Author: <a href="mailto:anthony.roberts@adelaide.edu.au">A.&nbsp;J. Roberts</a></dd>
+					<dd>Author: <a>A.&nbsp;J. Roberts</a></dd>
 				</dl>
 			</div>
 			<!--/#main-->
