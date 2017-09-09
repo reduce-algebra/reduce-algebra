73cc2575f4cbe05c5df0ff86b92d9e77382ba298
Francis Wright <fjwright@users.sourceforge.net>
2017-09-09 11:19:12 +0000

    Website: added a News page that automatically displays two SourceForge RSS feeds. It currently uses an external RSS processing web site, but I want to bring this in house ASAP.
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@4192 2bfe0521-f11c-4a00-b80e-6202646ff360

---
+++ htdocs/news.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta charset="utf-8" />
+	<title>REDUCE News</title>
+	<link rel="icon" type="image/png" href="images/icon.png" />
+	<link rel="stylesheet" href="StyleSheet.css" />
+	<style type="text/css">
+		div#main {
+			display: flex;
+			justify-content: space-around;
+		}
+
+		iframe.rsswidget {
+			border: 0;
+			padding: 0;
+			margin: 0;
+			width: 350px;
+			height: 500px;
+			border: 1px solid #333333;
+			background-color: #f8f8ff;
+		}
+	</style>
+</head>
+<body>
+	<div id="content">
+		<header>News</header>
+		<div id="middle">
+			<nav>
+				<script language="php">
+					include 'navigation.inc';
+				</script>
+			</nav>
+			<div id="main">
+				<iframe class="rsswidget" src="http://us1.rssfeedwidget.com/getrss.php?time=1504885544050&amp;x=https%3A%2F%2Fsourceforge.net%2Fp%2Freduce-algebra%2Fnews%2Ffeed.rss&amp;w=300&amp;h=500&amp;bc=333333&amp;bw=1&amp;bgc=transparent&amp;m=20&amp;it=true&amp;t=(default)&amp;tc=333333&amp;ts=15&amp;tb=transparent&amp;il=true&amp;lc=0000FF&amp;ls=14&amp;lb=false&amp;id=true&amp;dc=333333&amp;ds=14&amp;idt=true&amp;dtc=284F2D&amp;dts=12">Please use a browser that supports the HTML iframe element.</iframe>
+				<iframe class="rsswidget" src="http://us1.rssfeedwidget.com/getrss.php?time=1504886904716&amp;x=https%3A%2F%2Fsourceforge.net%2Fp%2Freduce-algebra%2Factivity%2Ffeed%3Fsource%3Dproject_activity&amp;w=300&amp;h=500&amp;bc=333333&amp;bw=1&amp;bgc=transparent&amp;m=20&amp;it=true&amp;t=(default)&amp;tc=333333&amp;ts=15&amp;tb=transparent&amp;il=true&amp;lc=0000FF&amp;ls=14&amp;lb=false&amp;id=true&amp;dc=333333&amp;ds=14&amp;idt=true&amp;dtc=284F2D&amp;dts=12">Please use a browser that supports the HTML iframe element.</iframe>
+			</div>
+			<!--/#main-->
+		</div>
+		<!--/#middle-->
+	</div>
+	<!--/#content-->
+</body>
+</html>
