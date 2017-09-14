bea2617737ee0c89bf67282991a9dd4f02409bd1
Francis Wright <fjwright@users.sourceforge.net>
2017-09-14 16:30:53 +0000

    Website: added a new page soliciting new developers to help improve REDUCE.
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@4205 2bfe0521-f11c-4a00-b80e-6202646ff360

---
+++ htdocs/helpwanted.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta charset="utf-8" />
+	<title>Help Wanted!</title>
+	<link rel="icon" type="image/png" href="images/icon.png" />
+	<link rel="stylesheet" href="StyleSheet.css" />
+	<style type="text/css">
+		span.reduce {
+			text-transform: capitalize;
+			font-variant: small-caps;
+		}
+	</style>
+</head>
+<body>
+	<div id="content">
+		<header>Help Wanted!</header>
+		<div id="middle">
+			<nav>
+				<script language="php">
+					include 'navigation.inc';
+				</script>
+			</nav>
+			<div id="main">
+				<p>Contributors to the <span class="reduce">Reduce</span> project are warmly welcome! This page is meant to attract the attention of volunteers on several issues that have arisen in the <span class="reduce">Reduce</span> developers' community. Interested people should subscribe to the developers' mailing list and post an item indicating where they would like to help.</p>
+				<dl>
+					<dt>State of <span class="reduce">Reduce</span> algorithms</dt>
+					<dd>It is necessary to check <span class="reduce">Reduce</span> against its main competitors, like Mathematica and Maple. This means checking if the main algorithms in <span class="reduce">Reduce</span> are still effective in comparison with those of other Computer Algebra Systems. Examples: <span class="reduce">Reduce</span> performs very well in simplifying rational expressions to the canonical form. However, <span class="reduce">Reduce</span> seems to perform badly in some packages which might be outdated, for example computing the Jordan canonical form of symbolic matrices (package linalg).</dd>
+
+					<dt>Bringing <span class="reduce">Reduce</span> to mobile devices</dt>
+					<dd>There is some code that is aimed at a Lisp interpreter in Java. The code proved to be partly working in an Android environment. This should be completed to a fully functional Android <span class="reduce">Reduce</span>.</dd>
+
+					<dt>Parallelizing <span class="reduce">Reduce</span></dt>
+					<dd>As nearly all devices have multiple cores, it is desirable to use them in <span class="reduce">Reduce</span>. For this one needs a Lisp interpreter that is able to run parallel tasks on the various cores. At the moment there is not even an idea about the model for such an interpreter.</dd>
+
+					<dt><span class="reduce">Reduce</span> packages in Linux distributions</dt>
+					<dd><span class="reduce">Reduce</span> is not packaged in the main Linux distributions, i.e., Debian-based and Red Hat-based distributions. The source code already contains scripts to generate deb packages that should work on every Debian-based system; the packages have been converted into rpm by the tool alien, and they have been successfully tested in some Red Hat-based distributions. We are looking for Debian/Red Hat maintainers who might wish to include and maintain a <span class="reduce">Reduce</span> package for those Linux distributions.</dd>
+
+					<dt>CSL documentation</dt>
+					<dd>At the moment, the CSL interpreter of the Lisp dialect in which most of <span class="reduce">Reduce</span> is written is almost undocumented. Volunteers should work through the CSL sources listing every defined symbol and writing a paragraph to explain it. In the source code there is a start at putting embedded annotations to be extracted into a TeX document, but continuing that to completion has never been done.</dd>
+
+					<dt>Graphical User Interface in CSL</dt>
+					<dd>At present the CSL version uses the FOX GUI toolkit to support its windowed interface. This supports Unix and Windows well, but only runs on a Macintosh via the X11 server, and Macintosh people may view that as almost useless for them. An experimental GUI using the wxWidgets graphical libraries is already in the <span class="reduce">Reduce</span> source code. Volunteers are needed to complete this task.</dd>
+
+					<dt>Type-checking in <span class="reduce">Reduce</span></dt>
+					<dd>Type mismatch is one of the most frequent errors that symbolic mode programmers make, and at present there are no diagnostics that clearly flag that sort of mistake. There have been attempts to introduce type checking in <span class="reduce">Reduce</span>: the package assert and the package reduce4. Starting from revision 4133 of March 2017 type information can be inserted in the arguments of procedures and the declaration of variables. This is a start at a complete type checking system; however much has to be done.</dd>
+
+					<dt>Improve the "ed" command</dt>
+					<dd>Review this <span class="reduce">Reduce</span> command and make it useful in a modern computing environment, such as by making it invoke the user's choice of editor. This is probably not that huge a task and could provide a gentle induction into the developer community.</dd>
+
+					<dt>LALR parser as a new <span class="reduce">Reduce</span> package</dt>
+					<dd></dd>
+
+					<dt>Garbage Collection</dt>
+					<dd></dd>
+				</dl>
+
+			</div>
+			<!--/#main-->
+		</div>
+		<!--/#middle-->
+	</div>
+	<!--/#content-->
+</body>
+</html>
