26294980161bba1bf863d8b6175f59b88bbf6e48
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2017-09-14 08:11:42 +0000

    Ensure that libffi-devel is installed on Fedora.
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@4202 2bfe0521-f11c-4a00-b80e-6202646ff360

--- scripts/fedora-sanity-check.sh
+++ scripts/fedora-sanity-check.sh
@@ -1,7 +1,19 @@
 # /bin/bash
 
-# An attempt to install all packages needed for REDUCE development
-# on a Fedora Workstation spin.   *** Not really tested yet ***
+# Install all packages needed for REDUCE development
+# on a Fedora Workstation spin.
+
+# When I started with a minimal install of Fedora 26 I installed
+# vim, openssh and subversion manually - I could then fetch first
+# fetchreduce.sh from another of my machines and then use that to
+# invoke subversion to grab all of Reduce - including this script.
+# Running this script seemed to get enough installed that I could
+# configure and build Reduce. But PLEASE NOTE that Fedora packaging
+# could change without me being aware, or changes to Reduce might
+# add extra needs that I fail to include here, so if you get a build
+# failure because of some missing header file or library first try
+# to identify a package to install to satisfy the dependancy. Then report
+# to the Reduce mailing lists so tha thos script can catch up.
 
 sudo dnf -y group install C\ Development\ Tools\ and\ Libraries
 
@@ -24,6 +36,9 @@ sudo dnf -y group install Development\ Tools
 #   doxygen              git                 gettext 
 #   subversion           highlight           patch   
 
+sudo dnf -y install openssh
+sudo dnf -y install vim
+
 sudo dnf -y install gnuplot
 sudo dnf -y install ImageMagick
 
@@ -32,6 +47,8 @@ sudo dnf -y install libXext-devel
 
 sudo dnf -y install ncurses-devel
 sudo dnf -y install ncurses-compat-libs
+
+sudo dnf -y install libffi-devel
 
 sudo dnf -y install polyml
 
