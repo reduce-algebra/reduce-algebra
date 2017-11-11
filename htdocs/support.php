84f64c4cff69fd5addfff4e082867d1e71330b5a
Francis Wright <fjwright@users.sourceforge.net>
2017-11-11 15:44:16 +0000

    Website: Start to add Q&A-style information to the Support page.
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@4261 2bfe0521-f11c-4a00-b80e-6202646ff360

--- htdocs/support.php
+++ htdocs/support.php
@@ -6,6 +6,12 @@
 	<meta name="Description" content="Support available for REDUCE" />
 	<link rel="icon" type="image/png" href="images/icon.png" />
 	<link rel="stylesheet" href="StyleSheet.css" />
+	<style type="text/css">
+		dt {
+			margin-top: 1ex;
+			margin-bottom: 0.5ex;
+		}
+	</style>
 </head>
 <body>
 	<div id="content">
@@ -13,11 +19,19 @@
 		<div id="middle">
 			<nav>
 				<script language="php">
-						include 'navigation.inc';
+					include 'navigation.inc';
 				</script>
 			</nav>
 			<div id="main">
-				<p>Support for <a href="obtaining.php">obtaining and installing REDUCE</a> and for <a href="documentation.php">running REDUCE</a> is available from this web site. This page gathers together some other useful support links.</p>
+				<dl>
+					<dt>Why are there two versions of REDUCE?</dt>
+					<dd>REDUCE runs on <a href="documentation.php">Standard Lisp</a> and there are two versions of Standard Lisp that were developed primarily to support REDUCE, namely <a href="versions.php">PSL and CSL</a> (in that order chronologically). We only release distributions of REDUCE based on these two Lisps, although REDUCE can be implemented on other versions of Standard Lisp (and there is some experimental code to do this in the <a href="https://sourceforge.net/p/reduce-algebra/code/HEAD/tree/trunk/">repository</a>).</dd>
+					<dt>What user interfaces does REDUCE provide?</dt>
+					<dd>PSL REDUCE itself provides only a command-line interface (CLI). CSL REDUCE provides a graphical user interface (GUI) by default, but it provides a command-line interface if started with the <code>--nogui</code> option. <a href="https://www.gnu.org/software/emacs/emacs.html">GNU Emacs</a> provides a GUI for PSL and CSL REDUCE via the <a href="reduce-ide/">REDUCE IDE</a> package and <a href="http://www.texmacs.org/">GNU TeXmacs</a> provides a GUI for PSL and CSL REDUCE via a <a href="https://sourceforge.net/p/reduce-algebra/code/HEAD/tree/trunk/generic/texmacs/">plugin for running REDUCE</a>. There is code in the repository for two other packages, <a href="https://sourceforge.net/p/reduce-algebra/code/HEAD/tree/trunk/generic/redfront">redfront</a> and <a href="https://sourceforge.net/p/reduce-algebra/code/HEAD/tree/trunk/psl/xr/">XR</a>, that provide GUIs for PSL, but neither is currently distributed as a pre-built binary and XR is somewhat out of date.</dd>
+					<dt>What start-up options does REDUCE provide?</dt>
+					<dd>For CSL REDUCE, running <code>redcsl --nogui --help</code> at a command prompt (assuming <code>redcsl</code> is on your search path – see <a href="obtaining.php">Obtaining REDUCE</a>) lists the start-up options.</dd>
+				</dl>
+				<p>Support for <a href="obtaining.php">obtaining and installing REDUCE</a> and for <a href="documentation.php">running REDUCE</a> is available from this web site. Here are some other useful support links.</p>
 				<dl>
 					<dt><a href="http://sourceforge.net/projects/reduce-algebra/">Project home page</a></dt>
 					<dd>the home page for the REDUCE distributions</dd>
