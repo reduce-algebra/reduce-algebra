1bfa825a483e231800a5718f35c143e4a03a80bc
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2017-12-11 15:51:01 +0000

    Improved script to reset a checked out tree to match the repository.
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@4293 2bfe0521-f11c-4a00-b80e-6202646ff360

--- scripts/make-pristine.sh
+++ scripts/make-pristine.sh
@@ -1,41 +1,55 @@
 #! /bin/sh
 
-echo "This script is a DRASTIC one that is not expected to be useful in"
-echo "ordinary circumstances, and should NOT be used casually. Its job is to"
-echo "bring the local copy as closely into step with the files on subversion"
-echo "as possible. In doing this it will DELETE any locally-built files and"
-echo "DISCARD any local changes. So if you are a developer then use of this"
-echo "script really risks leading to loss of your work."
-echo " "
-echo "Are you certain that you wish to continue. Type YES in capitals if"
-echo "you are"
+printf "This script is a DRASTIC one that is not expected to be useful in\n"
+printf "ordinary circumstances, and should NOT be used casually. Its job is to\n"
+printf "bring the local copy as closely into step with the files on subversion\n"
+printf "as possible. In doing this it will DELETE any locally-built files and\n"
+printf "DISCARD any local changes. So if you are a developer then use of this\n"
+printf "script really risks leading to loss of your work.\n"
+printf "\n"
+printf "Are you certain that you wish to continue. Type YES in capitals if\n"
+printf "you are\n"
 
 read answer
 
 if test "x$answer" != "xYES"
 then
-  echo "OK, not files will have been damaged."
+  printf "OK, not files will have been damaged.\n"
   exit 1
 fi
 
 here="$0";while test -L "$here";do here=`ls -ld "$here" | sed 's/.*-> //'`;done
 here=`dirname "$here"`
-here=`cd "$here"; pwd -P`
-echo `dirname "$here"`
+here=`cd "$here/.."; pwd -P`
 
-echo $here
 save=`pwd`
 cd $here
 
-# Discard files that I expect to be places where thiings are built or that
-# are residues of a build.
+# This looks for files that you have in your directory that subversion does
+# not know of as copies of things from the main repository. I set up ready to
+# delete all such. This may include local log files, work in progress or
+# ANYTHING that you have put anywhere in this tree, and the idea of this
+# script is that it is DELETED. Please note that you have been warned!
 
-rm -rf cslbuild pslbuild generic/redlog/libedit* generic/redlog/csl generic/redlog/psl
+svn status | grep "^?" | sed -e "s/^[?]/rm -rf/g" | /bin/sh
 
-# Discard files whose names suggest that they are not part of the
-# distribution from Sourceforge.
+# Some files or directories will be flagged as sv:ignore. They will
+# not have been in the original archive, so I want to delete them too.
 
-find . \( -name config.cache -o -name \*.bak -o -name autogen.stamp \) -print | xargs rm -rf
+for x in `svn -R pl | \
+  sed 's/^P/%P/g' | \
+  tr '\n%' ' \n' | \
+  grep svn:ignore | \
+  sed "s/Properties on '//g" | \
+  sed "s/':.*//g"`
+do
+  cd "$x"
+  for y in `svn pg svn:ignore`
+  do
+    rm -rf $y
+  done
+  cd $here
+done
 
 # Unwind any local changes to files. Note that this LOSES any changes that
 # you might have made intentionally, and so is not something to do casually.
@@ -47,16 +61,13 @@ svn -R revert .
 
 svn update
 
-# This looks for files that you have in your directory that subversion does
-# not know of as copies of things from the main repository. I set up ready to
-# delete all such. This may include local log files, work in progress or
-# ANYTHING that you have put anywhere in this tree, and the idea of this
-# script is that it is DELETED. Please note that you have been warned!
-
-svn status | grep "^?" | sed -e "s/^[?]/rm -rf/g" | /bin/sh
-
 # At the end I HOPE that if you go
 #     svn status
 # if will not generate any reports at all. You should check that now.
+
+printf "svn status report:\n"
+svn status
+
+printf "Distribution directory cleaned up\n"
 
 cd $save
