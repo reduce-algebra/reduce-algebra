d21694d0b36dfcccbd968976ab189e913a669fac
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2018-06-04 15:49:50 +0000

    The previous system demanded that cygwin and cygwin64 had been installed in the default locations for them on the C drive. The changes in other-cygwin.cpp and the configure.ac files arrange to scan the Windows registry to find where cygwin might be even if it has been installed "somewhere silly".
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@4635 2bfe0521-f11c-4a00-b80e-6202646ff360

--- winbuild/make-cygalt.sh
+++ winbuild/make-cygalt.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 
-# I now NEED cygwin to be in c:\cygwin and the 64-bit version to be
-# in c:\cygwin64.
-
 # Scan the disc to find cygwin installations and create files cygalt.exe
 # to change between them.
 #
@@ -14,6 +11,7 @@
 #
 
 rm -f cygalt.exe
-i686-w64-mingw32-g++ other-cygwin.cpp -o cygalt
+i686-w64-mingw32-g++ -O3 other-cygwin.cpp -static -o cygalt
+i686-w64-mingw32-strip cygalt
 
 # end of script
--- winbuild/other-cygwin.cpp
+++ winbuild/other-cygwin.cpp
@@ -1,11 +1,11 @@
-// other-cygwin.cpp                     Copyright (C) A C Norman  2014-2017
+// other-cygwin.cpp                     Copyright (C) A C Norman  2014-2018
 
 //
 // This program is to be run from a DOS command prompt or any cygwin
 // shell, and it "changes gear" so that the command that it is
 // given as its arguments is run in a full cygwin context.
-// It should always run its target in the version of cygwin it is
-// nor run from - ie if launched from a 32-bit terminal it will
+// It is capable of running its target in the version of cygwin it is
+// not run from - ie if launched from a 32-bit terminal it can
 // run a 64-bit application and vice versa.
 // Note that for good behaviour in such usage that mixes 32 and 64-bit
 // cygwin working it will be important that file systems map so that
@@ -14,16 +14,26 @@
 // start /cygdrive/x/...
 // In particular BEWARE of home directories!
 //
-// You must compile this with i686-w64-mingw32-gc++. This now requires that
-// the 32 and 64-bit versions of cygwin have been installed in c:\cygwin and
-// c:\cygwin64.
+// You must compile this with i686-w64-mingw32-gc++.
+// For this to work you (obviously) must have both the 32 and 64-bit
+// versions of cygwin installed such that the current user can access them.
+// The code here will probe the Windows registry to try to find where they
+// have been installed - that will very often be in c:\cygwin and c:\cygwin64.
+// Anybody who has or once had multiple instances of cygwin installed (apart
+// from one 32-bit and one 64-bit) MAY be lucky but I will not make any
+// guarantees. 
 //
-// If compiled with "-DFORCE32" or "-DFORCE64" it will only change gear
-// if not in the context suggested...
-
+// When compiled without command line flags this executes the command it
+// is given in the opposite versio nof cygwin to whichever is is invoked
+// from. If it is compiled with FORCE32 or FORCE64 set it will ensure that
+// the command is executed in the specifed context.
+//
+// If called without any thing as a command to execure this will display
+// an indication of which versions of cygwin it believes are available.
+// that may be useful at configure time.
 
 /**************************************************************************
- * Copyright (C) 2017, Codemist.                         A C Norman       *
+ * Copyright (C) 2018, Codemist.                         A C Norman       *
  *                                                                        *
  * Redistribution and use in source and binary forms, with or without     *
  * modification, are permitted provided that the following conditions are *
@@ -52,7 +62,7 @@
  *************************************************************************/
 
 
-// $Id: other-cygwin.cpp 4121 2017-07-23 13:35:50Z arthurcnorman $
+// $Id: other-cygwin.cpp 4402 2018-01-28 13:45:22Z arthurcnorman $
 
 
 
@@ -66,65 +76,246 @@
 
 static bool run64;
 
-#define scygwin (run64 ? "c:\\cygwin64" : "c:\\cygwin")
-
 #define LONGEST_COMMAND 4096
 
-static char command[LONGEST_COMMAND];
+static char command[LONGEST_COMMAND] = {0};
 
-// I keep this as a separate function in case I need to insert extra
-// escape characters at any stage...
+// Here I have a string s and I want to end up with 's' in the command.
+// However I am in a context where the command is itself already being
+// written within (single) quotes, so I have a messy notation to use to
+// include the quote marks.
+
 void append_command(char *s)
-{   strcat(command, s);
+{   char *p = command;
+    while (*p != 0 && p < &command[LONGEST_COMMAND]) p++;
+    if (p > &command[LONGEST_COMMAND-16])
+    {   fprintf(stderr, "\n+++ Command line too long\n");
+        exit(1);
+    }
+    *p++ = '\\';
+    *p++ = '\"';
+    int c;
+    while ((c = *s++) != 0)
+    {   if (c == '&' || c == '\\' || c == '<' || c == '>' ||
+            c == '^' || c == '|') *p++ = '^';
+        if (c == '\"' || c == '\\') *p++ = '\\';
+        *p++ = c;
+        if (p > &command[LONGEST_COMMAND-16])
+        {   fprintf(stderr, "\n+++ Command line too long\n");
+            exit(1);
+        }
+    }
+    *p++ = '\\';
+    *p++ = '\"';
+    *p = 0;
 }
 
 // The following code was found in newsgroup postings, and I believe it was
 // intended to be available for re-use.
 
 // Find a process with a given id in a snapshot
+
 BOOL FindProcessID(HANDLE snap, DWORD id, LPPROCESSENTRY32 ppe)
-{
-  BOOL res;
-  ppe->dwSize = sizeof(PROCESSENTRY32); // (mandatory)
-  res = Process32First(snap, ppe);
-  while (res) {
-    if (ppe->th32ProcessID == id) {
-      return TRUE;
+{   BOOL res;
+    ppe->dwSize = sizeof(PROCESSENTRY32); // (mandatory)
+    res = Process32First(snap, ppe);
+    while (res)
+    {   if (ppe->th32ProcessID == id) return TRUE;
+        res = Process32Next(snap, ppe);
     }
-    res = Process32Next(snap, ppe);
-  }
-  return FALSE;
+    return FALSE;
 }
 
 // Get the parent process id of the current process
+
 BOOL GetParentProcessId(DWORD* parent_process_id)
+{   HANDLE hSnap;
+    PROCESSENTRY32 pe;
+    DWORD current_pid = GetCurrentProcessId();
+
+    // Take a snapshot of all Windows processes
+    hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (INVALID_HANDLE_VALUE == hSnap) return FALSE;
+
+    // Find the current process in the snapshot
+    if (!FindProcessID(hSnap, current_pid, &pe)) return FALSE;
+
+    // Close the snapshot
+    if (!CloseHandle(hSnap)) return FALSE;
+
+    *parent_process_id = pe.th32ParentProcessID;
+    return TRUE;
+}
+
+static char cygwin_root[256];
+static char cygwinized_root[256];
+
+bool find_cygwin(bool sixty_four)
 {
-  HANDLE hSnap;
-  PROCESSENTRY32 pe;
-  DWORD current_pid = GetCurrentProcessId();
-
-  // Take a snapshot of all Windows processes
-  hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-  if (INVALID_HANDLE_VALUE == hSnap) {
-    return FALSE;
-  }
-
-  // Find the current process in the snapshot
-  if (!FindProcessID(hSnap, current_pid, &pe)) {
-    return FALSE;
-  }
-
-  // Close the snapshot
-  if (!CloseHandle(hSnap)) {
-    return FALSE;
-  }
-
-  *parent_process_id = pe.th32ParentProcessID;
-  return TRUE;
+//  printf("look for %s\n", sixty_four ? "64-bit" : "32-bit");
+// Here I wish to find a cygwin installation (of the given word-length).
+// I will look in the registry under HKCU\Software\cygwin\Installations.
+// I will return true on success.
+    HKEY hKey;
+    cygwin_root[0] = 0;
+    if (RegOpenKeyEx(HKEY_CURRENT_USER,
+                     TEXT("Software\\cygwin\\Installations"),
+                     0,
+                     KEY_READ,
+                     &hKey) == ERROR_SUCCESS) 
+    {
+// If the key was not present then I do not seem to have any cygwin
+// copies at all. If I do I will want to see where they live, and before
+// I extract information from the registry I need to know how large the
+// records that I will extract might be.
+        DWORD n_name, n_data;        // max size of fields in registry
+        DWORD rc = RegQueryInfoKey(
+            hKey,                    // key handle
+            NULL,                    // buffer for class name
+            NULL,                    // size of class string
+            NULL,                    // reserved
+            NULL,                    // number of subkeys
+            NULL,                    // longest subkey size
+            NULL,                    // longest class string
+            NULL,                    // number of values for this key
+            &n_name,                 // longest value name
+            &n_data,                 // longest value data
+            NULL,                    // security descriptor
+            NULL);                   // last write time
+// I create space for the returned value plus a terminating NUL. If the
+// allocation fails then an exception will be raised an the program will
+// terminate.
+        TCHAR *name = new TCHAR [n_name+1];
+        BYTE *data = new BYTE [n_data+1];
+// Scan all the registry data associated with this key. The index i tracks
+// which entry is returned, but there is no guarantee of material being
+// send back to me in any particular order.
+        for (DWORD i=0;; i++)
+        {   DWORD type;
+            DWORD namesize = n_name+1;    // allow for terminating NUL
+            DWORD datasize = n_data+1;
+            rc = RegEnumValue(
+                hKey,                     // Key
+                i,                        // Index
+                name,                     // put name here
+                &namesize,                // name size
+                NULL,                     // Reserved - must be NULL
+                &type,                    // put type here
+                data,                     // put data here
+                &datasize);               // data size
+// The key error code here is ERROR_NO_MORE_ITEMS, but I will abandon my
+// scan on any error, and not alert users to just what it was.
+            if (rc != ERROR_SUCCESS) break;
+            if (type != REG_SZ) continue; // Ignore non SZ values
+// The data returned here might be something like
+//        "\??\C:\cygwin64"
+// where the "\??" part at the front is not an accident - it is s Windows
+// path component stressing that the part "C:" is what you expect it to
+// mean in Windows. WHile one can EXPECT that the final component will be
+// either cygwin or cygwin64 thers is no guarantee of that... nor that the
+// choice of 32 vs 64-bit cygwin relates neatly to the directory path.
+// To see if this is a plausible cygwin installation I will look for
+// xxx/bin/cygwin1.dll and xxx/bin/bash.exe and for each of those I will
+// look for PE headers that might confirm that they looked like a DLL and
+// and executable and on that basis work out whether they are 32 or 64-bit
+// variants.
+            char *p = (char *)data;
+//          printf("Try this: %s\n", p);
+// Verify that the data starts of "\??\x:\" as expected. Also ignore any
+// cases where the string is unreasonably long.
+            if (p[0] != '\\' ||
+                p[1] != '?' ||
+                p[2] != '?' ||
+                p[3] != '\\' ||
+                !isalpha(p[4]) ||
+                p[5] != ':' ||
+                p[6] != '\\' ||
+                strlen(p) > 200) continue;
+//          printf("%s\n", p);
+            char path1[256], path2[256];
+            sprintf(path1, "%s\\bin\\cygwin1.dll", p+4);
+            sprintf(path2, "%s\\bin\\bash.exe", p+4);
+//          printf("Investigate %s and %s\n", path1, path2);
+            FILE *f1 = fopen(path1, "rb");
+            FILE *f2 = fopen(path2, "rb");
+// If the file does not exist or is not readable then this is not a good
+// candidate.
+            if (f1 == NULL)
+            {   if (f2 != NULL) fclose(f2);
+                continue;
+            }
+            if (f2 == NULL)
+            {   fclose(f1);
+                continue;
+            }
+            IMAGE_DOS_HEADER dos_header1, dos_header2;
+            if (fread(&dos_header1, sizeof(dos_header1), 1, f1) == 1 &&
+                dos_header1.e_magic == IMAGE_DOS_SIGNATURE &&
+                fread(&dos_header2, sizeof(dos_header2), 1, f2) == 1 &&
+                dos_header2.e_magic == IMAGE_DOS_SIGNATURE)
+            {   LONG next1 = dos_header1.e_lfanew,
+                     next2 = dos_header2.e_lfanew;
+                IMAGE_NT_HEADERS nt_headers1, nt_headers2;
+                if (fseek(f1, next1, SEEK_SET) == 0 &&
+                    fread(&nt_headers1, sizeof(nt_headers1), 1, f1) == 1 &&
+                    nt_headers1.Signature == IMAGE_NT_SIGNATURE &&
+                    fseek(f2, next2, SEEK_SET) == 0 &&
+                    fread(&nt_headers2, sizeof(nt_headers2), 1, f2) == 1 &&
+                    nt_headers2.Signature == IMAGE_NT_SIGNATURE)
+                {
+//                  printf("read NT headers\n");
+//                  printf("machine1 = %x\n", nt_headers1.FileHeader.Machine);
+//                  printf("machine2 = %x\n", nt_headers2.FileHeader.Machine);
+// The machine type is expected to be either IMAGE_FILE_MACHINE_I386 for
+// a 32-bit image or IMAGE_FILE_MACHINE_AMD64 for 64-bit.
+                    bool ok;
+                    if (sixty_four)
+                        ok = nt_headers1.FileHeader.Machine ==
+                                 IMAGE_FILE_MACHINE_AMD64 &&
+                             nt_headers2.FileHeader.Machine ==
+                                 IMAGE_FILE_MACHINE_AMD64;
+                    else
+                        ok = nt_headers1.FileHeader.Machine ==
+                                 IMAGE_FILE_MACHINE_I386 &&
+                             nt_headers2.FileHeader.Machine ==
+                                 IMAGE_FILE_MACHINE_I386;
+// Wow - now  may have found a nice installation. Well there is some messy
+// possibility that I might find several locations corresponding to
+// multiple cygwin installations. I think that ought not to be the situation,
+// but I can still worry about it. I will keep the one where its path
+// is shortest, and if there are several with the same path length I will
+// keep the one that comes alphabetically earliest. This should mean that
+// I prefer (eg) C:\cygwin to D:\cygwin and both of those to C:\old\cygwin.
+                    if (ok &&
+                        (cygwin_root[0] == 0 ||
+                         (strlen(p+4) <= strlen(cygwin_root) &&
+                          strcmp(p+4, cygwin_root) < 0)))
+                          strcpy(cygwin_root, p+4);
+                }
+            }
+            fclose(f1);
+            fclose(f2);
+        }
+        delete [] data;
+    }
+    RegCloseKey(hKey);
+    return cygwin_root[0] != 0;
 }
 
 int main(int argc, char *argv[])
 {
+    if (argc == 1)
+    {   bool c32 = find_cygwin(false);
+        bool c64 = find_cygwin(true);
+// In general I will be expecting that this program is called from a cygwin
+// shell...
+        printf("%s\n",
+            !c32 && !c64 ? "No cygwin installation at all found!\n" :
+            !c32 && c64  ? "64" :
+            c32 && !c64  ? "32" :
+                           "32 64");
+        return 0;
+    }
     run64 = false;
     DWORD parent;
     if (!GetParentProcessId(&parent))
@@ -148,33 +339,43 @@ int main(int argc, char *argv[])
     {   SYSTEM_INFO s;
         GetNativeSystemInfo(&s);
         switch (s.wProcessorArchitecture)
-        {
-        case PROCESSOR_ARCHITECTURE_AMD64:
-            // printf("parent is a 64-bit process\n");
-            run64 = false;
-            break;
-        case PROCESSOR_ARCHITECTURE_INTEL:
+        {   case PROCESSOR_ARCHITECTURE_AMD64:
+                // printf("parent is a 64-bit process\n");
+                run64 = false;
+                break;
+            case PROCESSOR_ARCHITECTURE_INTEL:
 #ifndef FORCE32
-            printf("You seem to be running 32-bit operating system\n");
-            printf("This utility can not work. Exiting\n");
-            return 1;
+                printf("You seem to be running 32-bit operating system\n");
+                printf("This utility can not work. Exiting\n");
+                return 1;
 #else
-            break;
+                break;
 #endif
-        default:
-            printf("Unknown platform: exiting.\n");
-            return 1;
+            default:
+                printf("Unknown platform: exiting.\n");
+                return 1;
         }
     }
 
 // At this stage run64 is set to indicate the opposite of the shell currently
-// in use.
+// in use. That would be the default shell that one wants to end up in.
+// If one of the FORCE options is enabled then override that dynamic test.
+    bool running64 = !run64;
 
 #if defined FORCE64
     run64 = true;
 #elif defined FORCE32
     run64 = false;
 #endif
+
+// Now I know which version of cygwin I want to run a command under, and
+// so I will check what versions are available.
+
+    if (!find_cygwin(run64))
+    {   printf("No %d bit version of cygwin found. Sorry\n",
+               run64 ? 64 : 32);
+        return 1;
+    }
 
     STARTUPINFO startup;
     PROCESS_INFORMATION process;
@@ -219,25 +420,44 @@ int main(int argc, char *argv[])
     user = getenv("USER");
     if (user == NULL) user = "unknown";
     memset(newenv, 0, sizeof(newenv));
-    if (run64)
-        sprintf(newenv, "OTHER=yes%cUSER=%s%cPATH=/cygdrive/c/cygwin64/bin%c", 0, user, 0, 0);
-    else
-        sprintf(newenv, "OTHER=yes%cUSER=%s%cPATH=/cygdrive/c/cygwin/bin%c", 0, user, 0, 0);
+// What I do here is to create a string that contains
+//    windir\system32\cmd /c
+//         "c:\cygwin64\bin\bash --login -c
+//          \"cd DIRECTORY; c1; c2; ...\""
+// so first "cmd /c "..." will remove the first and final double quotes
+// on the line but leave all the rest exactly as is.
+// Then "bash -c '...'" uses the single quotes there to arrange that a single
+// string is provided as an operand for "-c". This will involve a sequence of
+// items that must each parse as a single word, and which (being within single
+// quotes) must have escapes and quote marks in them protected.
+// Well within the '...' if an embedded single quote is required it must be
+// rendered as '\'' - that ends the string, puts in an individual single quote
+// by escaping it with a backslash and then resumes the string.
+//
+// I need a variant on cygwin_root converted from x:\... to /cygdrive/x/...
+    sprintf(cygwinized_root, "/cygdrive/%c%s",
+         cygwin_root[0], &cygwin_root[2]);
+    for (char *p=cygwinized_root; *p!=0; p++)
+        if (*p == '\\') *p = '/';
+    sprintf(newenv, "OTHER=yes%cUSER=%s%cPATH=%s/bin%c",
+        0, user, 0, cygwinized_root, 0);
     for (i=0; i<dirsize; i++)
         if (current[i] == '\\') current[i] = '/';
     sprintf(command,
-        "%s\\%s\\cmd /c \"%s\\bin\\bash --login -c \'cd ",
-        getenv("WINDIR"), run64 ? "sysnative" : "system32", scygwin);
+            "%s\\%s\\cmd /s /d /c %s\\bin\\bash --login -c \\\"cd ",
+            getenv("WINDIR"), run64 ? "sysnative" : "system32", cygwin_root);
     append_command(current);
     strcat(command, " ; ");
     for (i=1; i<argc; i++)
     {   append_command(argv[i]);
-        strcat(command, " ");
+        if (i != argc-1) strcat(command, " ");
     }
-    strcat(command, "\'\"");
-//  printf("Command is <%s>\n", command); fflush(stdout);
+    strcat(command, "\"");
+#if 0
+    printf("Command is <%s>\n", command); fflush(stdout);
+#endif
     char newdir[1024];
-    sprintf(newdir, "%s\\bin", scygwin);
+    sprintf(newdir, "%s\\bin", cygwin_root);
     rc = CreateProcess(
              NULL,                       // ApplicationName
              command,                    // Command line
