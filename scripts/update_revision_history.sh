8ff2524f9c8a4c1574b038edc6b7be7b27e5a337
Rainer Schöpf <schoepf@users.sourceforge.net>
2019-11-03 06:59:21 +0000

    scripts/update_revision_history.sh can now be run from any directory, allows to write to another file if given as parameter.
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@5183 2bfe0521-f11c-4a00-b80e-6202646ff360

--- scripts/update_revision_history.sh
+++ scripts/update_revision_history.sh
@@ -1,2 +1,14 @@
 #! /bin/sh
-svn log -q -rHEAD:4454 |sed -n -e 's/^r\([0-9]*\) | .* | \([0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}\) .*$/Revision \1 : date \2/p' >revision_history.txt
+
+output=revision_history.txt
+test -z "$1" || output="$1"
+
+# This script updates the revisions_history.txt file in the Reduce toplevel directory.
+# Without a parameter it writes directly to revisions_history.txt.
+# To write to another file, pass its path as parameter.
+here="$0";while test -L "$here";do here=`ls -ld "$here" | sed 's/.*-> //'`;done
+here=`dirname "$here"`
+here=`cd "$here/.."; pwd -P`
+cd $here
+
+svn log -q -rHEAD:4454 |sed -n -e 's/^r\([0-9]*\) | .* | \([0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}\) .*$/Revision \1 : date \2/p' >"$output"
