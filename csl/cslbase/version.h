3bdfeb919d5d5e16c6a070af2e812217321ec9ac
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2020-04-29 15:38:05 +0000

    CSL: fixes to the config files so that sigaction gets detected properly on the latext Xcode on a Mac. Well I am not exactly certain when things broke but when I tested today "struct sigaction" was not properly detected]. Also more dependencies to show that u01.cpp etc depend on CSL header files.
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@5341 2bfe0521-f11c-4a00-b80e-6202646ff360

--- csl/cslbase/version.h
+++ csl/cslbase/version.h
@@ -39,7 +39,7 @@
 // (scripts/commit.sh) that is used to update the subversion repository to
 // update the revision number here.
 
-#define REVISION 5339
+#define REVISION 5341
 
 #endif // header_version_h
 
