6e127e6f1172469a18eb2cb79461dd2f14c4235c
Avery Laird <a-work@users.sourceforge.net>
2020-11-18 22:16:11 +0000

    integrating reduce web version into build process
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@5504 2bfe0521-f11c-4a00-b80e-6202646ff360

---
+++ csl/new-embedded/for-emscripten/js/io.js
@@ -0,0 +1,24 @@
+mergeInto(LibraryManager.library, {
+    web_async_read: function () {
+        return Asyncify.handleAsync(async () => {
+            // out("waiting for input");
+            const char = await read_char_promise();
+            // out(char);
+            return char;
+        });
+    },
+    web_async_write: function (char) {
+        // console.log('async write called: ')
+        // out(intArrayToString([char]));
+        outputBuf.push(char);
+        if (intArrayToString([char]) === '\n') {
+            term.write(outputBuf);
+            outputBuf = [];
+        }
+        return 0;
+    },
+    web_async_info_write: function (char) {
+        out(`read char: ${char}, ${intArrayToString([char])}`);
+        return 0;
+    }
+});
