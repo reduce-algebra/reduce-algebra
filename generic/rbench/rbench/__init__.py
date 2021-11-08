aadf381bd12c5b05dd5f06d16ea7ede151dbc31e
Thomas Sturm <thomas-sturm@users.sourceforge.net>
2021-11-08 15:22:31 +0000

    Revised OO. Things are cleaner now.
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@6157 2bfe0521-f11c-4a00-b80e-6202646ff360

--- generic/rbench/rbench/__init__.py
+++ generic/rbench/rbench/__init__.py
@@ -8,6 +8,6 @@ ___copyright__ = 'Copyright 2021, Thomas Sturm, Germany'
 ___license__ = 'CC BY-NC-ND'
 ___version___ = '$Rev$'
 
-from .analytics import Benchmark
+from .analytics import RbDataFrame
 from .analytics import combine2
 from .analytics import read_filetree
