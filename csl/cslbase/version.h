5f8abf5d7b135160e4aae312d60565193341b6ce
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2022-07-24 20:23:59 +0000

    CSL: remove an unwanted disgnostic print of stray "#" characters that had crept into the previous checkin.
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@6357 2bfe0521-f11c-4a00-b80e-6202646ff360

--- csl/cslbase/version.h
+++ csl/cslbase/version.h
@@ -39,7 +39,7 @@
 // (scripts/commit.sh) that is used to update the subversion repository to
 // update the revision number here.
 
-#define REVISION 6356
+#define REVISION 6357
 
 #endif // header_version_h
 
