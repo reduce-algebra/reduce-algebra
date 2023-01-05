3e1b61b3de52926c582adadc3702a34be9d13e4e
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2023-01-05 21:43:19 +0000

    CSL: A further attempt to get arithlib.hpp working properly and to refine it wrt speed. Still needs more testing especially on all possible platforms.
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@6481 2bfe0521-f11c-4a00-b80e-6202646ff360

--- csl/cslbase/version.h
+++ csl/cslbase/version.h
@@ -39,7 +39,7 @@
 // (scripts/commit.sh) that is used to update the subversion repository to
 // update the revision number here.
 
-#define REVISION 6479
+#define REVISION 6480
 
 #endif // header_version_h
 
