28f1ab3536f606feb9d5a5286fd63cb3f17a9948
Rainer Schöpf <schoepf@users.sourceforge.net>
2023-02-04 07:48:54 +0000

    Get revision number in Reduce startup banner from svnversion output, even during snapshot build.
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@6512 2bfe0521-f11c-4a00-b80e-6202646ff360

--- scripts/revision.sh
+++ scripts/revision.sh
@@ -6,8 +6,23 @@ here="$0";while test -L "$here";do here=`ls -ld "$here" | sed 's/.*-> //'`;done
 here=`dirname "$here"`
 here=`cd "$here"; pwd -P`
 
-a=`svnversion -c $here/..`
-a=${a%M}
+# If called from an unversioned directory svnversion prints a message
+# that depends on the current language. Set LANG=C to always print the
+# english message.
+LANG=C
+
+# If a file called svnversion.txt exists in the toplevel directory, use
+# its contents instead of svnversion's output. 
+svnversiontxt=$here/../svnversion.txt
+if test -r $svnversiontxt
+then
+  a=$(cat $svnversiontxt)
+else
+  a=$(svnversion -c $here/..)
+fi
+
+# Remove message and trailing modifiers from svnversion output.
+a=`echo $a | grep -v 'Unversioned\|Uncommitted' | sed -e 's/[A-Z]*$//'` 
 a=${a#*:}
 echo $a
 
