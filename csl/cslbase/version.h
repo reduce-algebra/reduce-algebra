237b956ac288e31e8608f4adc0caaed3668bcc51
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2023-02-27 09:18:07 +0000

    A change to how I build a universal libffi for the Macintosh that will (I hope) make things work better on different OS releases. Specifically I hope it will let me build the univeral library on an intel Mac running Big Sur.
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@6541 2bfe0521-f11c-4a00-b80e-6202646ff360

--- csl/cslbase/version.h
+++ csl/cslbase/version.h
@@ -39,7 +39,7 @@
 // (scripts/commit.sh) that is used to update the subversion repository to
 // update the revision number here.
 
-#define REVISION 6536
+#define REVISION 6537
 
 #endif // header_version_h
 
--- packages/support/revision.red
+++ packages/support/revision.red
@@ -31,6 +31,6 @@
 
 fluid '(revision!*);
 
-revision!* := 6536;
+revision!* := 6537;
 
 end;
