d785924435f34af6e01c3ed71aae534813a20857
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2024-12-01 15:28:46 +0000

    Add a scripts that MAY help if you wish to use brew rather than ports on a macintosh - but note that this is not really tested so please view it as a staryimng point not a complete solution
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@6894 2bfe0521-f11c-4a00-b80e-6202646ff360

--- packages/support/revision.red
+++ packages/support/revision.red
@@ -31,6 +31,6 @@
 
 fluid '(revision!*);
 
-revision!* := 6893;
+revision!* := 6894;
 
 end;
---
+++ scripts/brew-sanity-check.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+brew install subversion
+brew install astyle autoconf ccache git gnuplot libedit libffi ncurses
+
