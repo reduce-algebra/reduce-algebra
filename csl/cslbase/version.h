055edc05f9a39b1049d0a86235c29378104f9bb8
Jeffrey H. Johnson <trnsz@pobox.com>
2025-09-01 04:37:17 -0400

    Update to "$Id: version.h 7169 2025-09-01 08:37:17Z reduce $"

--- csl/cslbase/version.h
+++ csl/cslbase/version.h
@@ -44,7 +44,7 @@
 
 #include <cstdio>
 
-#define VERSION_ID "$Id: version.h 7169 2025-08-31 08:35:51Z reduce $"
+#define VERSION_ID "$Id: version.h 7169 2025-09-01 08:37:17Z reduce $"
 
 // Extract the revision number.
 
