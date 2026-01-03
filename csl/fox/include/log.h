28316d9f7a1a4986c5baadbb6c76461fc969596f
Arthur C. Norman <arthurcnorman@users.sourceforge.net>
2026-01-03 09:31:10 +0000

    I subversion was being fussy about replacing symbolic links with files, so this is a second attempt at that.
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@7233 2bfe0521-f11c-4a00-b80e-6202646ff360

--- csl/fox/include/log.h
+++ csl/fox/include/log.h
@@ -1,1 +1,19 @@
-../../cslbase/log.h
+// log.h                                                    A C Norman 2026
+
+// This file might have been a symbolic link to cslbase/log.h but
+// I fwish to use msys2 as a development environment on Windoes sometimes
+// and its support for symbolic links is restricted by the the fact that
+// Windows and Linux do things differently. So by default the "ln -s"
+// command makes a deep copy rather than setting up a link. By this use of
+// #include I get the effect of the single version of the file subject
+// to concerns about path searches etc.
+
+// This file should be considered to be covered by the same BSD license that
+// most of the files in cslbase use. I have not included the full text since
+// it is over-bulky for such a silly and tiny file!
+
+// $Id:
+
+#include "../../cslbase/log.h"
+
+// end of log.h
