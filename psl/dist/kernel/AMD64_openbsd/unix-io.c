45673ad1a5008b2bf6e49a5f9fbe1a630132c073
Rainer Schöpf <schoepf@users.sourceforge.net>
2026-03-31 07:34:10 +0000

    Minor change to PSL kernel code
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@7351 2bfe0521-f11c-4a00-b80e-6202646ff360

--- psl/dist/kernel/AMD64_openbsd/unix-io.c
+++ psl/dist/kernel/AMD64_openbsd/unix-io.c
@@ -1,75 +1,75 @@
 /*
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
-% File:         PXK:UNIX-IO.C
-% Description:  Unix PSL FileDescriptors are implemented as stdio streams
-%                 ("FILE *".)
-% Author:       Russell D. Fish
-% Created:      Thu Feb 16 1984
-% Modified:     17-Jul-84 22:49:12 (RAM)
-% Mode:         Text
-% Package:
-% Status:       Open Source: BSD License
-%
-% (c) Copyright 1983, Hewlett-Packard Company, see the file
-%            HP_disclaimer at the root of the PSL file tree
-%
-% (c) Copyright 1982, University of Utah
-%
-% Redistribution and use in source and binary forms, with or without
-% modification, are permitted provided that the following conditions are met:
-%
-%    * Redistributions of source code must retain the relevant copyright
-%      notice, this list of conditions and the following disclaimer.
-%    * Redistributions in binary form must reproduce the above copyright
-%      notice, this list of conditions and the following disclaimer in the
-%      documentation and/or other materials provided with the distribution.
-%
-% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-% THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-% PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNERS OR
-% CONTRIBUTORS
-% BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-% POSSIBILITY OF SUCH DAMAGE.
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
-% Revisions:
-%
-% 15-Sep-88 (T. Yamamoto and C. Burdorf)
-%  Moved collect out of expand_file_name so it won't be overwritten as a
-%  local before it is referenced.
-% 20-Sep-86 (Leigh Stoller)
-%  Removed assembler alias statements because they are not portable. Instead,
-%  a sed script will be used to convert the _variables of C to VARIABLES of
-%  PSL.
-% 17-Jul-84 22:48:32 (RAM)
-%  Added unixcd, a routine that calls expand_file_name before calling chdir.
-% 3-Jul-84 10:45:57 (RAM)
-%  Added expand_file_name (called from unixopen) to expand shell variable
-%  references and ~ home directory syntax.
-% 29-Jun-84 14:21:21 (RAM)
-%  Added unixputs and unixopen.
-% 21-May-84 17:41:41 (Vicki O'Day)
-%  Added unixcleario.  It is called by syscleario.
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
-%  $Id: unix-io.c 6307 2022-05-11 15:35:54Z schoepf $
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-*/
+ *
+ * File:         PXK:UNIX-IO.C
+ * Description:  Unix PSL FileDescriptors are implemented as stdio streams
+ *                 ("FILE *".)
+ * Author:       Russell D. Fish
+ * Created:      Thu Feb 16 1984
+ * Modified:     17-Jul-84 22:49:12 (RAM)
+ * Mode:         Text
+ * Package:
+ * Status:       Open Source: BSD License
+ *
+ * (c) Copyright 1983, Hewlett-Packard Company, see the file
+ *            HP_disclaimer at the root of the PSL file tree
+ *
+ * (c) Copyright 1982, University of Utah
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *    * Redistributions of source code must retain the relevant copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNERS OR
+ * CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ *****************************************************************************
+ *
+ * Revisions:
+ *
+ * 15-Sep-88 (T. Yamamoto and C. Burdorf)
+ *  Moved collect out of expand_file_name so it won't be overwritten as a
+ *  local before it is referenced.
+ * 20-Sep-86 (Leigh Stoller)
+ *  Removed assembler alias statements because they are not portable. Instead,
+ *  a sed script will be used to convert the _variables of C to VARIABLES of
+ *  PSL.
+ * 17-Jul-84 22:48:32 (RAM)
+ *  Added unixcd, a routine that calls expand_file_name before calling chdir.
+ * 3-Jul-84 10:45:57 (RAM)
+ *  Added expand_file_name (called from unixopen) to expand shell variable
+ *  references and ~ home directory syntax.
+ * 29-Jun-84 14:21:21 (RAM)
+ *  Added unixputs and unixopen.
+ * 21-May-84 17:41:41 (Vicki O'Day)
+ *  Added unixcleario.  It is called by syscleario.
+ *
+ *******************************************************************************
+ *
+ *  $Id: unix-io.c 7247 2026-01-09 12:11:30Z schoepf $
+ *
+ *******************************************************************************
+ */
  
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
+#include <pwd.h>
 
 /* There is an assumption here that coercing addresses into ints is OK */
 /*
@@ -87,7 +87,7 @@ extern FILE * unixstdin, * unixstdout, * unixstderr, * unixtty;
 
 /* Import NULL and EOF constants for error returns from stdio fns.
  */
-extern int unixnull[2], unixeof[2];
+extern long long unixnull, unixeof;
 
 /* Tag( unixinitio )
  */
@@ -97,10 +97,8 @@ unixinitio()
     unixstdin = stdin;
     unixstdout = stdout;
     unixstderr = stderr;
-    unixnull[0] = (long) NULL;
-    unixnull[1] = (long) NULL;
-    unixeof[0] = EOF;
-    unixeof[1] = EOF;
+    unixnull = (long)NULL;
+    unixeof = EOF;
     unixtty = fopen("/dev/tty", "r");
 }
  
@@ -128,7 +126,7 @@ unixputn(long long n)
 {
     fprintf(stdout, "%llx", n);
 }
- 
+
 /* Tag( unixcleario )
  */
 void
@@ -155,19 +153,17 @@ unixcleario()
    values supplied by the shell environment.  The file name is first copied
    to a temporary copy because the parsing algorithm must write into the
    string (this may change).  For now, the maximum string length supported
-   is 255 characters, but no checking is done to see if this is exceeded.
+   is MAXPATHLEN characters, but no checking is done to see if this is exceeded.
    $ and ~ variables are expanded in one pass by calling the functions getenv,
    getuid, getpwuid, and getpwnam.  If any expansion fails, the original
    string is returned.
 */
- 
-#include <pwd.h>
-struct passwd *getpwuid(uid_t);
-struct passwd *getpwnam(const char *);
-char *getenv(const char*);
 
-char collect[255], copy[255];  /* Made global so it won't be overwritten
-                  Used to be local to expand_file_name */
+#include "sys/param.h"	/* For MAXPATHLEN, the longest path that can occur. */
+
+/* Made global so it won't be overwritten
+ * Used to be local to expand_file_name */
+char collect[MAXPATHLEN + 2], copy[MAXPATHLEN + 2];
  
 /* Tag( expand_file_name )
  */
@@ -226,11 +222,10 @@ char *expand_file_name(char *fname)
   return (collect);
 }
  
-extern int errno;
  
-FILE* unixopen(char *filename, char *type)
+FILE *unixopen(char *filename, char *type)
 {
-  FILE* fptr;
+  FILE *fptr;
  
   fptr = fopen(expand_file_name(filename), type);
   return(fptr);
@@ -244,8 +239,9 @@ unixcd(char *filename)
 
 int
 unixfclose (FILE *ix)
-
-{ return fclose (ix); }
+{
+  return fclose (ix);
+}
 
 int
 external_system(char *command)
@@ -276,19 +272,20 @@ copy_argv(int argc,char *argv[])    /* copy argv into static space. */
   return(static_argv);
 }
 
+static char static_resolved_path[MAXPATHLEN + 2];
+
 /* convert a pathname to canonical form */
 char *
 external_fullpath(char *relpath)
 {
-  return realpath(relpath,NULL);
+  return realpath(relpath,static_resolved_path);
 }
 
 long long xgetw (FILE *f)
-{ long long a1,a2;
+{
+  long long a1,a2;
 
   a1 = (long long) getw(f);
   a2 = (long long) getw(f);
   return (a2 << 32 | a1);
 }
-
-
