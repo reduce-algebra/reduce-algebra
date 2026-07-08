0f1610bec579906960fb0902f85ca450b8793c09
Jeffrey H. Johnson <johnsonjh.dev@gmail.com>
2026-07-08 04:44:49 -0400

    Update to "$Id: version.h 7375 2026-07-08 08:44:48Z reduce $"

--- csl/cslbase/version.h
+++ csl/cslbase/version.h
@@ -44,7 +44,7 @@
 
 #include <cstdio>
 
-#define VERSION_ID "$Id: version.h 7375 2026-07-07 08:47:43Z reduce $"
+#define VERSION_ID "$Id: version.h 7375 2026-07-08 08:44:48Z reduce $"
 
 namespace CSL_LISP
 {
