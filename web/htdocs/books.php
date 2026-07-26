0b1eaeff4bdaeb11a042970878a09d82bc1c80d6
Francis Wright <fjwright@users.sourceforge.net>
2026-07-26 16:26:07 +0000

    Web Site: Books
    Add another book about REDUCE and update some URLs, which should all
    now use https.
    
    
    git-svn-id: https://svn.code.sf.net/p/reduce-algebra/code/trunk@7387 2bfe0521-f11c-4a00-b80e-6202646ff360

--- web/htdocs/books.php
+++ web/htdocs/books.php
@@ -66,7 +66,7 @@ include './include/begin-body.php';
 
 <?php $primary_books = array(
 
-    array('url' => 'http://www.cambridge.org/gb/academic/subjects/physics/particle-physics-and-nuclear-physics/using-reduce-high-energy-physics?format=PB#si7TZq9PBUEIGGG9.97',
+    array('url' => 'https://www.cambridge.org/gb/academic/subjects/physics/particle-physics-and-nuclear-physics/using-reduce-high-energy-physics?format=PB#si7TZq9PBUEIGGG9.97',
           'img' => '<img src="images/Grozin.jpg" width="180" height="259" alt="" />',
           'ttl' => '<span class="title">Using REDUCE in High Energy Physics</span>',
           'dsc' => '<span class="authors">by A. G. Grozin</span>
@@ -79,7 +79,7 @@ include './include/begin-body.php';
           'dsc' => '<span class="authors">by N. MacDonald</span>
 <span class="biblio">168 pages, CRC Press, ISBN 9780750302777, 1994</span>'),
 
-    array('url' => 'http://www.springer.com/gb/book/9783540567059',
+    array('url' => 'https://link.springer.com/book/10.1007/978-3-642-78227-5',
           'img' => '<img src="images/Hehl-Winkelmann-Meyer.jpg" width="153" height="235" alt="" />',
           'ttl' => '<span class="title">REDUCE</span>',
           'dsc' => '<span class="subtitle" lang="de">Ein Kompaktkurs &uuml;ber die Anwendung von Computer-Algebra</span>
@@ -91,9 +91,15 @@ include './include/begin-body.php';
           'ttl' => '<span class="title" lang="de">Einf&uuml;hrung in die Computeralgebra mit REDUCE</span>',
           'dsc' => '<span class="subtitle" lang="de">f&uuml;r Mathematiker, Informatiker und Physiker</span>
 <span class="authors" lang="de">by Johannes Ueberberg</span>
-<span class="biblio">331 pages, BI-Wiss.-Verlag, ISBN 9783411157815, 1992</span>'),
+<span class="biblio">331 pages, BI-Wissenschaftsverlag, ISBN 9783411157815, 1992</span>'),
 
-    array('url' => 'http://www.springer.com/gb/book/9780792314417',
+    array('url' => 'https://www.abebooks.co.uk/Algorithms-Computation-Reduce-Steeb-Willi-Hans-Dirk/31287990651/bd',
+          'img' => '<img src="images/Steeb-Lewien.jpg" width="198" height="300" alt="" />',
+          'ttl' => '<span class="title">Algorithms and Computation with REDUCE</span>',
+          'dsc' => '<span class="authors">by Willi-Hans Steeb and Dirk Lewien</span>
+<span class="biblio">153 pages, BI-Wissenschaftsverlag, ISBN 9783411156511, 1992</span>'),
+
+    array('url' => 'https://link.springer.com/book/10.1007/978-94-011-3502-3',
           'img' => '<img src="images/Brackx-Constales.jpg" width="153" height="232" alt="" />',
           'ttl' => '<span class="title">Computer Algebra with LISP and REDUCE</span>',
           'dsc' => '<span class="subtitle">An Introduction to Computer-aided Pure Mathematics</span>
@@ -106,7 +112,7 @@ include './include/begin-body.php';
           'dsc' => '<span class="authors">by Malcolm A. H. MacCallum and Francis J. Wright</span>
 <span class="biblio">314 pages, Oxford University Press, ISBN 9780198534433, 1991</span>'),
 
-    array('url' => 'http://www.springer.com/math/cse/book/978-0-387-96598-7',
+    array('url' => 'https://link.springer.com/book/10.1007/978-1-4612-4806-4',
           'img' => '<img src="images/Rayna.jpg" width="153" height="232" alt="" />',
           'ttl' => '<span class="title">REDUCE</span>',
           'dsc' => '<span class="subtitle">Software for Computer Algebra</span>
@@ -134,40 +140,40 @@ foreach ($primary_books as $book): ?>
           'dsc' => '<span class="authors">by Michael Reed</span>
 <span class="biblio">545 pages, Lulu, ISBN 9798994367216, 2025</span>'),
 
-    array('url' => 'http://www.springer.com/la/book/9783319716541',
+    array('url' => 'https://link.springer.com/book/10.1007/978-3-319-71655-8',
           'img' => '<img src="images/Krasilshchik-Verbovetsky-Vitolo.jpg" width="153" height="232" alt="" />',
           'ttl' => '<span class="title">The Symbolic Computation of Integrability Structures<br />for Partial Differential Equations</span>',
           'dsc' => '<span class="authors">by Joseph Krasil&apos;shchik, Alexander Verbovetsky and Raffaele Vitolo</span>
 <span class="biblio">250 pages, Springer, ISBN 9783319716558, 2017</span>'),
 
-    array('url' => 'https://my.siam.org/Store/Product/viewproduct/?ProductId=26052157',
+    array('url' => 'https://epubs.siam.org/doi/book/10.1137/1.9781611973563',
           'img' => '<img id="roberts" src="images/Roberts.jpg" width="153" height="232" alt="" />',
           'ttl' => '<span class="title">Model Emergent Dynamics in Complex Systems</span>',
           'dsc' => '<span class="authors">by A. J. Roberts</span>
 <span class="biblio">748 pages, SIAM, ISBN 9781611973556, 2014</span>'),
 
-    array('url' => 'http://www.springer.com/gb/book/9783540654667',
+    array('url' => 'https://link.springer.com/book/10.1007/978-3-642-55826-9',
           'img' => '<img src="images/Grabmeier-Kaltofen-Weispfenning.jpg" width="153" height="232" alt="" />',
           'ttl' => '<span class="title">Computer Algebra Handbook</span>',
           'dsc' => '<span class="subtitle">Foundations &middot; Applications &middot; Systems</span>
 <span class="authors">edited by Johannes Grabmeier, Erich Kaltofen and Volker Weispfenning</span>
 <span class="biblio">637 pages, Springer, ISBN 9783642629884, 2003</span>'),
 
-    array('url' => 'http://www.worldscientific.com/worldscibooks/10.1142/2362',
+    array('url' => 'https://www.worldscientific.com/worldscibooks/10.1142/2362#t=aboutBook',
           'img' => '<img src="images/Steeb.jpg" width="120" height="168" alt="" />',
           'ttl' => '<span class="title">Quantum Mechanics Using Computer Algebra</span>',
           'dsc' => '<span class="subtitle">Includes Sample Programs for REDUCE, MAPLE, MATHEMATICA and C++</span>
 <span class="authors">by Willi-Hans Steeb</span>
 <span class="biblio">200 pages, World Scientific, ISBN 9789810217709, 1994</span>'),
 
-    array('url' => 'http://www.springer.com/gb/book/9783540565307',
+    array('url' => 'https://link.springer.com/book/10.1007/978-3-642-78117-9',
           'img' => '<img src="images/Stauffer-Hehl-Ito-Winkelmann-Zabolitzky.jpg" width="153" height="232" alt="" />',
           'ttl' => '<span class="title">Computer Simulation and Computer Algebra</span>',
           'dsc' => '<span class="subtitle">Lectures for Beginners</span>
 <span class="authors">by D. Stauffer, Friedrich W. Hehl, Nobuyasu Ito, Volker Winkelmann and John G. Zabolitzky</span>
 <span class="biblio">287 pages, Springer, ISBN 9783540565307, 1993</span>'),
 
-    array('url' => 'http://www.worldscientific.com/worldscibooks/10.1142/2097',
+    array('url' => 'https://www.worldscientific.com/worldscibooks/10.1142/2097#t=aboutBook',
           'img' => '<img src="images/Marti.jpg" width="120" height="168" alt="" />',
           'ttl' => '<span class="title">RLISP &#39;88</span>',
           'dsc' => '<span class="subtitle">An Evolutionary Approach to Program Design and Reuse</span>
